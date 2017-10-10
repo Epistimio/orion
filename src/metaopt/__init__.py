977e2d37aa7d9ce11546c5c960a6fa230dbce5b5
Christos Tsirigotis <tsirif@gmail.com>
2017-10-09 20:55:02 -0400

    Add packages, package_dir and scripts in setup.py

--- src/metaopt/__init__.py
+++ src/metaopt/__init__.py
@@ -2,11 +2,4 @@ from ._version import get_versions
 versions = get_versions()
 del get_versions
 
-#  if versions['error']:
-#      raise RuntimeError(versions['error'])
-
 __version__ = versions['version']
-
-print(versions)
-print()
-print(__version__)
