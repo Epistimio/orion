4d30ab5e8ea9ede2448905f561dde3e3bc611bc7
Christos Tsirigotis <tsirif@gmail.com>
2017-11-06 13:15:19 -0500

    Modify setup to handle metadata and config files
    - Draw package metadata info from `metaopt` module (__init__ file)
    - Setup configuration files from `config` to `<sys.prefix>/share/config`
    - Setup LICENSE, README.md to `<sys.prefix>/share`

--- setup.py
+++ setup.py
@@ -1,24 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import sys
+import os
+from glob import iglob
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import (setup, find_packages)
 
 import versioneer
 
-setup(name='metaopt',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='Hyperparameter Optimization',
-      long_description=open('README.md').read(),
-      license='BSD-3-Clause',
-      author='MetaOptimizer Team - MILA/Université de Montréal',
-      author_email='lisa_labo@iro.umontreal.ca',
-      url='https://github.com/mila-udem/metaopt',
-      packages=['metaopt'],
-      package_dir={'': 'src'},
-      scripts=['scripts/mopt'],
-      install_requires=[],
-      )
+isfile = os.path.isfile
+pjoin = os.path.join
+repo_root = os.path.dirname(os.path.abspath(__file__))
+mpath = pjoin(repo_root, 'src')
+sys.path.insert(0, mpath)
+
+import metaopt
+
+
+def find_data_files():
+    """Find MetaOpt's configuration and metadata files"""
+    install_config_path = pjoin(metaopt.dirs.site_data_dir, 'config')
+    config_path = pjoin('config', '*')
+    configs = [cfg for cfg in iglob(config_path) if isfile(cfg)]
+
+    data_files = [
+        (install_config_path, configs),
+        (metaopt.dirs.site_data_dir, ['LICENSE', 'README.md']),
+        ]
+
+    return data_files
+
+
+setup_args = dict(
+    name=metaopt.__name__,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description=metaopt.__descr__,
+    long_description=metaopt.__doc__,
+    license=metaopt.__license__,
+    author=metaopt.__author__,
+    author_email=metaopt.__author_email__,
+    url=metaopt.__url__,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=find_data_files(),
+    scripts=['scripts/mopt'],
+    install_requires=[],
+    setup_requires=[],
+    )
+
+
+setup_args['keywords'] = [
+    'Machine Learning',
+    'Deep Learning',
+    'Distributed',
+    'Optimization',
+    ]
+
+setup_args['platforms'] = ['Linux']
+
+setup_args['classifiers'] = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
+
+
+if __name__ == "__main__":
+    setup(**setup_args)
