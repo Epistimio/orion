59df51b216ac6c774f85df6fea15b32af2a192ba
Christos Tsirigotis <tsirif@gmail.com>
2017-11-12 14:40:10 -0500

    Add `tests_require`, `setup_requires` in setup.py
    - Depend on pytest, pytest_runner for tests, setup.
    - Lint file for flake8+import-order+docstrings

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Installation script for MetaOpt."""
 from __future__ import absolute_import
-import sys
-import os
-import textwrap
-from glob import iglob
 
-from setuptools import (setup, find_packages)
+from glob import iglob
+import os
+import sys
+import textwrap
+
+from setuptools import (find_packages, setup)
 
 import versioneer
 
@@ -16,67 +18,66 @@ repo_root = os.path.dirname(os.path.abspath(__file__))
 mpath = pjoin(repo_root, 'src')
 sys.path.insert(0, mpath)
 
-import metaopt
+import metaopt  # noqa
 
 
 def find_data_files():
-    """Find MetaOpt's configuration and metadata files"""
+    """Find MetaOpt's configuration and metadata files."""
     install_config_path = pjoin(metaopt.dirs.site_data_dir, 'config')
     config_path = pjoin('config', '*')
     configs = [cfg for cfg in iglob(config_path) if isfile(cfg)]
 
     data_files = [
-        (install_config_path, configs),
-        (metaopt.dirs.site_data_dir, ['LICENSE', 'README.md']),
-        ]
+            (install_config_path, configs),
+            (metaopt.dirs.site_data_dir, ['LICENSE', 'README.md']),
+            ]
 
     return data_files
 
 
 setup_args = dict(
-    name=metaopt.__name__,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description=metaopt.__descr__,
-    long_description=textwrap.dedent(metaopt.__doc__),
-    license=metaopt.__license__,
-    author=metaopt.__author__,
-    author_email=metaopt.__author_email__,
-    url=metaopt.__url__,
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    data_files=find_data_files(),
-    scripts=['scripts/mopt'],
-    install_requires=['six', 'PyYAML'],
-    setup_requires=[],
-    )
-
+        name=metaopt.__name__,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        description=metaopt.__descr__,
+        long_description=textwrap.dedent(metaopt.__doc__),
+        license=metaopt.__license__,
+        author=metaopt.__author__,
+        author_email=metaopt.__author_email__,
+        url=metaopt.__url__,
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        include_package_data=True,
+        data_files=find_data_files(),
+        scripts=['scripts/mopt'],
+        install_requires=['six', 'PyYAML'],
+        tests_require=['pytest'],
+        setup_requires=['pytest-runner'],
+        )
 
 setup_args['keywords'] = [
-    'Machine Learning',
-    'Deep Learning',
-    'Distributed',
-    'Optimization',
-    ]
+        'Machine Learning',
+        'Deep Learning',
+        'Distributed',
+        'Optimization',
+        ]
 
 setup_args['platforms'] = ['Linux']
 
 setup_args['classifiers'] = [
-    'Development Status :: 1 - Planning',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Education',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: POSIX',
-    'Operating System :: Unix',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-    'Topic :: Scientific/Engineering',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    ]
-
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        ]
 
 if __name__ == '__main__':
     setup(**setup_args)
