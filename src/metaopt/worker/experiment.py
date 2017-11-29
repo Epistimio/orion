3801e95b53366b41b72b06f8d3c5b3464fc9e19d
Christos Tsirigotis <tsirif@gmail.com>
2017-11-29 11:43:29 -0500

    Init Experiment and Trial classes

---
+++ src/metaopt/worker/experiment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`metaopt.worker.experiment` -- Description of an optimization attempt
+==========================================================================
+
+.. module:: trial
+   :platform: Unix
+   :synopsis: Manage history of trials corresponding to a black box process
+
+"""
+
+
+class Experiment(object):
+    pass
---
+++ src/metaopt/worker/trial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`metaopt.worker.trial` -- Container class for `Trial` entity
+=================================================================
+
+.. module:: trial
+   :platform: Unix
+   :synopsis: Describe a particular training run, hyperparameters and results
+
+"""
+
+
+class Trial(object):
+    pass
