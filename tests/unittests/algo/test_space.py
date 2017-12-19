1ade68304110a6c6c81a0d4d6eeef6fd20b34c11
Christos Tsirigotis <tsirif@gmail.com>
2017-12-19 15:10:20 -0500

    Move test directories around to group unittests better

---
+++ tests/unittests/algo/test_space.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`metaopt.core.io.database`."""
+
+import pytest
+
+from metaopt.algo.space import (Categorical, Dimension, Integer, Real, Space)
