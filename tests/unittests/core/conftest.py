70dcbb8aa2f241a4516bad984e68f30e17a7c8cd
Christos Tsirigotis <tsirif@gmail.com>
2018-03-28 22:39:38 -0400

    Client: Add helper function for manual insertion of parameter sets
    Why:
    A user might want to populate a collection of trials with one's own
    choices of set of parameters for a particular experiment. Call it
    intuition, call it bias, call it experience; the user should be able to
    act as if one was the Producer itself.
    
    How:
    User can either write a second script of one's own, where some tuples of
    appropriate sets of parameters are calculated and fed to
    `metaopt.client.insert_trials`, or one can do the same thing from
    iPython - for example. The function is written as part of the
    `metaopt.client` interface to facilitate this interaction.
    
    In order to work, an `Experiment` object must be instantiated, and this
    need in turn a `Database` object to lie in the Python process. So user
    is asked to pass necessarily the experiment's name which the trials will
    be associated with, and take care of a way to discover the Database. The
    latter will be done in the same manner that `metaopt.core.cli.infer_experiment`
    discovers the `Database`. That is either by default locations,
    environmental variables, or an external configuration. The last one is
    enabled by allowing an extra optional keyword argument in the helper
    function, ``cmdconfig``, which is expected to have the key 'database'
    which is a dictionary with configuration details, as usual.
    
    Note:
    The experiment must exist already as a configuration in the specified
    database. **So, currently pre-population of an experiment's new trials is
    not supported**.
    
    Finally:
    All points suggested by the user are checked against the experiment's
    parameter space. In case of error, an optional argument controls whether
    an `AssertionError` will be raised or the failing parameter sets will be
    ignored.

--- tests/unittests/core/conftest.py
+++ tests/unittests/core/conftest.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 """Common fixtures and utils for tests."""
 
-import datetime
-import getpass
 import os
 
 import pytest
@@ -10,20 +8,12 @@ import pytest
 from orion.algo.space import (Categorical, Integer, Real, Space)
 from orion.core.io.convert import (JSONConverter, YAMLConverter)
 from orion.core.io.database import Database
-from orion.core.io.database.mongodb import MongoDB
 from orion.core.utils import SingletonError
 from orion.core.worker.experiment import Experiment
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 YAML_SAMPLE = os.path.join(TEST_DIR, 'sample_config.yml')
 JSON_SAMPLE = os.path.join(TEST_DIR, 'sample_config.json')
-
-
-@pytest.fixture()
-def null_db_instances():
-    """Nullify singleton instance so that we can assure independent instantiation tests."""
-    Database.instance = None
-    MongoDB.instance = None
 
 
 @pytest.fixture()
@@ -76,38 +66,6 @@ def space():
 def fixed_suggestion():
     """Return the same tuple/sample from a possible space."""
     return (('asdfa', 2), 0, 3.5)
-
-
-@pytest.fixture()
-def with_user_tsirif(monkeypatch):
-    """Make ``getpass.getuser()`` return ``'tsirif'``."""
-    monkeypatch.setattr(getpass, 'getuser', lambda: 'tsirif')
-
-
-@pytest.fixture()
-def with_user_bouthilx(monkeypatch):
-    """Make ``getpass.getuser()`` return ``'bouthilx'``."""
-    monkeypatch.setattr(getpass, 'getuser', lambda: 'bouthilx')
-
-
-@pytest.fixture()
-def with_user_dendi(monkeypatch):
-    """Make ``getpass.getuser()`` return ``'dendi'``."""
-    monkeypatch.setattr(getpass, 'getuser', lambda: 'dendi')
-
-
-@pytest.fixture()
-def random_dt(monkeypatch):
-    """Make ``datetime.datetime.utcnow()`` return an arbitrary date."""
-    random_dt = datetime.datetime(1903, 4, 25, 0, 0, 0)
-
-    class MockDatetime(datetime.datetime):
-        @classmethod
-        def utcnow(cls):
-            return random_dt
-
-    monkeypatch.setattr(datetime, 'datetime', MockDatetime)
-    return random_dt
 
 
 @pytest.fixture()
