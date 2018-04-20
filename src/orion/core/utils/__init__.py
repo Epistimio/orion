cc40132f755fa27aee0be0086017c453ab5f4b5d
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2018-04-20 17:20:18 -0400

    Update __init__.py
    More cosmetic underlining

--- src/orion/core/utils/__init__.py
+++ src/orion/core/utils/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 :mod:`orion.core.utils` -- Package-wide useful routines
-=========================================================
+=======================================================
 
 .. module:: utils
    :platform: Unix
