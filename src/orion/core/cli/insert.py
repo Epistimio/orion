93d18a79baf3ad31a4a0ee2883fa6e685f06eb85
François Corneau-Tremblay <corneau90@gmail.com>
2018-05-02 10:44:47 -0400

    Add `insert` command to generate a single trial
    Why: The `insert` command let's the user insert a single trial
    with specific values. This is useful in cases where the hyperparameters
    are approximately known or if the user is testing an implementation
    of someone else's model and the hyperparameters are known.
    
    How: The user gives the different point through the command line
    following closely the same convention as for priors' distibution
    by giving the namespace then its value like this : "-x=1". The
    insert command then makes sure of multiple things:
    
        -It first checks that there is a default value for all the
        dimensions inside the experiment's space that are not specified
        by the user.
    
        -Then it checks that there is no namespace outside of the
        experiment space.
    
        -Finally, it makes sure that the given value lies inside the
        prior's distribution interval
    
    If these conditions are respected, a new trial is inserted
    inside the database.

--- src/orion/core/cli/insert.py
+++ src/orion/core/cli/insert.py
@@ -20,6 +20,7 @@ from orion.core.io.database import Database
 from orion.core.cli import resolve_config
 from orion.core.worker.experiment import Experiment
 from orion.core.io.convert import infer_converter_from_file_type
+from orion.core.utils.format_trials import tuple_to_trial
 
 log = logging.getLogger(__name__)
 
@@ -45,11 +46,44 @@ def fetch_args(args):
     execute(args, config)
 
 
-def execute(cmdargs, cmdconfig):
-    points = cmdargs.pop('user_args', None)
-    experiment, cmdargs = infer_experiment(cmdargs, cmdconfig)
+def execute(cmd_args, file_config):
+    command_line_user_args = cmd_args['metadata'].pop('user_args', None)
+    experiment = infer_experiment(cmd_args, file_config)
 
-def infer_experiment(cmdargs, cmdconfig):
+    if experiment.status is None:
+        raise RuntimeError("No experiment with this name inside "
+                            "the database.")
+
+    transformed_args = build_from(command_line_user_args)
+    exp_space = SpaceBuilder().build_from(experiment.configuration['metadata']['user_args'])
+
+    validate_dimensions(transformed_args, exp_space)
+
+    values = [value for namespace, value in transformed_args.items()]
+
+    trial = tuple_to_trial(values, exp_space)
+
+    experiment.register_trials([trial])
+
+def validate_dimensions(transformed_args, exp_space):
+    exp_namespaces = list(exp_space.keys())
+    
+    #Find any dimension that is not given by the user and make sure they have a default value
+    for exp_n in exp_namespaces:
+        if exp_n not in transformed_args.keys() and exp_space[exp_n].default_value is None: 
+            error_msg = "Dimension {} is unspecified and has no default value".format(invalid_namespaces)
+            raise ValueError(error_msg)
+    
+    #Find any namespace that is not in the space of the experiment, or values that lie outside the prior's interval
+    for namespace, value in transformed_args.items():
+        if namespace not in exp_space:
+            error_msg = "Found namespace outside of experiment space : {}".format(namespace)
+            raise ValueError(error_msg)
+        elif value not in exp_space[namespace]:
+            error_msg = "Value {} is outside of dimension's prior interval".format(value)
+            raise ValueError(error_msg)
+
+def infer_experiment(cmd_args, file_config):
     # Initialize configuration dictionary.
     # Fetch info from defaults and configurations from default locations.
     expconfig = resolve_config.fetch_default_options()
@@ -61,7 +95,8 @@ def infer_experiment(cmdargs, cmdconfig):
 
     # Initialize singleton database object
     tmpconfig = resolve_config.merge_orion_config(expconfig, dict(),
-                                                  cmdconfig, cmdargs)
+                                                  file_config, cmd_args)
+    
     db_opts = tmpconfig['database']
     dbtype = db_opts.pop('type')
     
@@ -75,12 +110,12 @@ def infer_experiment(cmdargs, cmdconfig):
                            "Please use either `name` cmd line arg or provide "
                            "one in orion's configuration file.")
 
-    experiment = create_experiment(exp_name, expconfig, cmdconfig, cmdargs)
+    experiment = create_experiment(exp_name, expconfig, file_config, cmd_args)
 
-    return experiment, cmdargs
+    return experiment
 
 
-def create_experiment(exp_name, expconfig, cmdconfig, cmdargs):
+def create_experiment(exp_name, expconfig, file_config, cmd_args):
     """Create an experiment based on configuration.
 
     Configuration is a combination of command line, experiment configuration
@@ -109,26 +144,15 @@ def create_experiment(exp_name, expconfig, cmdconfig, cmdargs):
     dbconfig = experiment.configuration
 
     expconfig = resolve_config.merge_orion_config(expconfig, dbconfig,
-                                                  cmdconfig, cmdargs)
+                                                  file_config, cmd_args)
 
     # Infer rest information about the process + versioning
     expconfig['metadata'] = infer_versioning_metadata(expconfig['metadata'])
-
+    
     # Pop out configuration concerning databases and resources
     expconfig.pop('database', None)
     expconfig.pop('resources', None)
     expconfig.pop('status', None)
-
-    # Finish experiment's configuration and write it to database.
-    try:
-        experiment.configure(expconfig)
-    except DuplicateKeyError:
-        # Fails if concurrent experiment with identical (name, metadata.user)
-        # is written first in the database.
-        # Next infer_experiment() should either load experiment from database
-        # and run smoothly if identical or trigger an experiment fork.
-        # In other words, there should not be more than 1 level of recursion.
-        experiment = create_experiment(exp_name, expconfig, cmdconfig, cmdargs)
 
     return experiment
 
@@ -140,4 +164,89 @@ def infer_versioning_metadata(existing_metadata):
     # User repo's HEAD commit hash
     return existing_metadata
 
-    
+def build_from(cmd_args):
+    transformed_args, userconfig, is_userconfig_an_option = build_from_args(cmd_args)
+
+    if userconfig:
+        transformed_args = build_from_config(userconfig)
+
+    return transformed_args
+
+def build_from_config(config_path):
+    converter = infer_converter_from_file_type(config_path)
+    userconfig_tmpl = converter.parse(config_path)
+    USERCONFIG_KEYWORD = 'orion='
+
+    transformed_args = {}
+    stack = collections.deque()
+    stack.append(('', userconfig_tmpl))
+    while True:
+        try:
+            namespace, stuff = stack.pop()
+        except IndexError:
+            break
+        if isinstance(stuff, dict):
+            for k, v in stuff.items():
+                stack.append(('/'.join([namespace, str(k)]), v))
+        elif isinstance(stuff, list):
+            for position, thing in enumerate(stuff):
+                stack.append(('/'.join([namespace, str(position)]), thing))
+        elif isinstance(stuff, str):
+            if stuff.startswith(USERCONFIG_KEYWORD):
+                if namespace in transformed_args:
+                    error_msg = "Conflict for name '{}' in script configuration "\
+                                "and arguments.".format(namespace)
+                    raise ValueError(error_msg) from exc
+                
+                transformed_args[namespace] = stuff[len(USERCONFIG_KEYWORD):]
+
+    return transformed_args
+
+def build_from_args(cmd_args):
+    userconfig = None
+    is_userconfig_an_option = None
+    USERARGS_SEARCH = r'\W*([a-zA-Z0-9_-]+)=([a-zA-Z0-9_]+)'
+    USERARGS_TMPL = r'(.*)=(.*)'
+
+    transformed_args = {}
+    userargs_tmpl = collections.defaultdict(list)
+    args_pattern = re.compile(USERARGS_SEARCH)
+    args_prefix_pattern = re.compile(USERARGS_TMPL)
+
+    for arg in cmd_args:
+        found = args_pattern.findall(arg)
+        if len(found) != 1:
+            if arg.startswith(self.USERCONFIG_OPTION):
+                if not userconfig:
+                    userconfig = arg[len(self.USERCONFIG_OPTION):]
+                    is_userconfig_an_option = True
+                else:
+                    raise ValueError(
+                        "Already found one configuration file in: %s" %
+                        userconfig
+                        )
+            else:
+                self.userargs_tmpl[None].append(arg)
+            continue
+
+        name, value = found[0]
+        namespace = '/' + name
+
+        if namespace in transformed_args:
+            error_msg = "Conflict for name '{}' in script configuration "\
+                        "and arguments.".format(namespace)
+            raise ValueError(error_msg) from exc
+        
+        transformed_args[namespace] = value
+
+        found = args_prefix_pattern.findall(arg)
+        assert len(found) == 1 and found[0][1] == value, "Parsing prefix problem."
+        userargs_tmpl[namespace] = found[0][0] + '='
+
+    if not userconfig and userargs_tmpl[None]:  # try the first positional argument
+        if os.path.isfile(self.userargs_tmpl[None][0]):
+            userconfig = userargs_tmpl[None].pop(0)
+            is_userconfig_an_option = False
+
+    return transformed_args, userconfig, is_userconfig_an_option
+
