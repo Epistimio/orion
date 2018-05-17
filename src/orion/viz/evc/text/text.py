b2ec9d680c26081d6d6ca341fddfb585117c16ba
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-05-17 13:03:56 -0400

    to-be squashed

--- src/orion/viz/evc/text/text.py
+++ src/orion/viz/evc/text/text.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-:mod:`orion.viz.evc.graph` -- Build and image graph based on exp-fork tree
+:mod:`orion.viz.evc.text` -- Build and text graph based on exp-fork tree
 ==============================================================================
 
 .. module:: vizualization
    :platform: Unix
-   :synopsis: Obtains graph object and outputs an image for research purposes
+   :synopsis: Obtains graph object and outputs an text-based graph for research 
+              purposes
 
 """
 import networkx
 import matplotlibt
-
---
+++ tests/unittests/viz/test_image_graph.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`orion.viz.evc.graph`."""
