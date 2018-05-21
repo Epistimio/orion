451b38bc6532a8b6e1672794743ac220973d07d4
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-05-21 12:59:06 -0400

    adding skeleton for image graph and text graph testing

--- tests/unittests/viz/test_image_graph.py
+++ tests/unittests/viz/test_image_graph.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Example usage and tests for :mod:`orion.viz.evc.graph`."""
+
+import pytest
+
+from orion.viz.evc.graph import graph_vizualize
+
+@pytest.fixture()
+def create_image_graph():
+    pass
+
+@pytest.fixture()
+def test_create_image_graph():
+    """
+    Thinking out loud:
+        so I thought of using the creation of the graphs for tests.
+        Given two identical graphs and given the similar procedure
+        that the graphs are generated. The MD5 hash of the graphs
+        woud be similar to each other. In this case we would load
+        the graph into memory, hash the whole image into a MD5
+        string. This assertion would logically be the correct
+        graph generated to the one we would want.
+    """
+    pass
