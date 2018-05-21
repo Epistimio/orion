27e22e08a3480beb7a96a060264e95c68fb62088
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-05-21 13:13:52 -0400

    Update `Experiment` based on new refers definition
    Why:
    
    The definition of `refers` is now:
    
     - `root_id`: Experiment id of the root experiment of the EVC tree
        This helps improve efficiency on db fetches since we can fetch a
        tree simply by specifying the root_id. It is also simple to keep
        coherent compared to a double-linked tree with (parent, children)
        links.
     - `parent_id`: Parent id of the current experiment
     - `adapter`: CompositeAdaptor definition to make trials from parent
        compatible with the current one and vice-versa.
    
    How:
    
    On creation of new experiments, `refers` is updated after the experiment
    is added to database such that `refers[root_id]` can point to itself.
    
    Note:
    
    Because `refers` contain sub-dictionaries which are edited after
    configuration (adapters instantiation), we need to use deep copies of
    the configuration when configuring.

--- tests/unittests/core/test_experiment.py
+++ tests/unittests/core/test_experiment.py
@@ -152,7 +152,7 @@ class TestInitExperiment(object):
         assert exp._db is create_db_instance
         assert exp._id is None
         assert exp.name == 'supernaekei'
-        assert exp.refers is None
+        assert exp.refers == {}
         assert exp.metadata['user'] == 'tsirif'
         assert exp.metadata['datetime'] == random_dt
         assert exp._last_fetched == random_dt
@@ -172,7 +172,7 @@ class TestInitExperiment(object):
         assert exp._db is create_db_instance
         assert exp._id is None
         assert exp.name == 'supernaedo2'
-        assert exp.refers is None
+        assert exp.refers == {}
         assert exp.metadata['user'] == 'bouthilx'
         assert exp.metadata['datetime'] == random_dt
         assert exp._last_fetched == random_dt
@@ -226,7 +226,7 @@ class TestConfigProperty(object):
         exp = Experiment('supernaekei')
         cfg = exp.configuration
         assert cfg['name'] == 'supernaekei'
-        assert cfg['refers'] is None
+        assert cfg['refers'] == {}
         assert cfg['metadata']['user'] == 'tsirif'
         assert cfg['metadata']['datetime'] == random_dt
         assert len(cfg['metadata']) == 2
@@ -302,7 +302,7 @@ class TestConfigProperty(object):
         _id = found_config[0].pop('_id')
         assert _id != 'fasdfasfa'
         assert exp._id == _id
-        new_config['refers'] = None
+        new_config['refers'] = {}
         new_config['status'] = 'pending'
         new_config.pop('_id')
         new_config.pop('something_to_be_ignored')
@@ -311,9 +311,10 @@ class TestConfigProperty(object):
         new_config['algorithms']['dumbalgo']['scoring'] = 0
         new_config['algorithms']['dumbalgo']['suspend'] = False
         new_config['algorithms']['dumbalgo']['value'] = 5
+        new_config['refers'] = {'adapter': [], 'parent_id': None, 'root_id': _id}
         assert found_config[0] == new_config
         assert exp.name == new_config['name']
-        assert exp.refers is None
+        assert exp.configuration['refers'] == new_config['refers']
         assert exp.metadata == new_config['metadata']
         assert exp.pool_size == new_config['pool_size']
         assert exp.max_trials == new_config['max_trials']
@@ -645,7 +646,7 @@ class TestInitExperimentView(object):
         assert exp._experiment._db._database is create_db_instance
         assert exp._id == exp_config[0][0]['_id']
         assert exp.name == exp_config[0][0]['name']
-        assert exp.refers == exp_config[0][0]['refers']
+        assert exp.configuration['refers'] == exp_config[0][0]['refers']
         assert exp.metadata == exp_config[0][0]['metadata']
         assert exp._experiment._last_fetched == exp_config[0][0]['metadata']['datetime']
         assert exp.pool_size == exp_config[0][0]['pool_size']
