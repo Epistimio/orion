80b5a96b66baf0f0a518acdd157957138414a50a
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-05-22 16:10:52 -0400

    finishing implementation of graph visualization

--- src/orion/viz/evc/graph/graph.py
+++ src/orion/viz/evc/graph/graph.py
@@ -12,22 +12,23 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
+
+from networkx.drawing.nx_agraph import to_agraph
+from networkx.drawing.nx_agraph import graphviz_layout
 
 class evc_graph(object):
     r"""Image graph object
     
-    Nodes are given as EVC nodes
-
-    Given these EVC nodes we would check if the node is the current root if 
-    we would want to create a the graph of the whole EVC tree. But if not
-    given the root or a specific node, we would want to print out just the 
+    Nodes are given as EVC nodes. Given these EVC nodes we would check
+    if the node is the current root if we would want to create a the
+    graph of the whole EVC tree. But if not given the root or a specific
+    node, we would want to print out just the
     subgraph of the EVC tree.
     
     """
-    def __init__(self):
-        super(evc_graph, self, evc_node).__init__()
+    def __init__(self, evc_node):
         """Initialize the image graph
 
         """
@@ -37,27 +38,23 @@ class evc_graph(object):
         an EVC node is passed through.
         '''
         self.evc_node = evc_node
-        
+
+        self.evc_root = self.evc_node.root
+        self.root_item = self.evc_root.item
         '''
         Create a networkx graph and allocate
         it in memory
         '''
-        self.graph = nx.Graph()
+        self.graph = nx.DiGraph()
 
         '''
         Build graph based on
         EVC nodes. Would it better be a 
         property of the class?
         '''
-        self.build_graph(root=True)
+        self.build_graph(root=False)
 
-    def add_node(self, node, children):
-        self.graph.add_node(node)
-        for child in children:
-            self.graph.add_edge(node, child)
-        return node, children
-
-    def build_graph(self, root=True):
+    def build_graph(self, root=False):
         """Convert the EVC tree to the networkx graph
         
         Algorithm:
@@ -70,167 +67,113 @@ class evc_graph(object):
         """
 
         if root:
-            self.evc_node = self.evc_node.root()
-        
-        '''
-        Traverse the tree preorderly and then add 
-        EVC node to the nn.Graph() graph
-        '''
+            '''
+            If we would want to choose to print the whole EVC tree
+            choose as root=True so that we would start traversing
+            the whole tree with the root.
+            '''
+            self.evc_node.root
 
-        self.evc_node.map(self.add_node, self.evc_node.children)
-    
-    def dumps_tikz(self, layout='layered', use_label=True):
-        """Return TikZ code as `str`given a graph
+            '''
+            Traverse the tree preorderly and then add 
+            EVC node to the nn.Graph() graph
+            '''
+            for node in self.evc_node:
+                self.graph.add_node(node.item)
+                for child in node.children:
+                    self.graph.add_edge(node.item, child.item)
         
-        """
-        '''
-        adjusting layout
-        '''
-        if layout not in ('layered', 'spring'):
-            raise ValueError('Unknown layout: {s}'.format(s=layout))
-        '''
-        start at \0 string
-        '''
-        s = ''
-        for n, d in self.graph.nodes_iter(data=True):
-            # label
-            label = self.graph.node[n].get('label', '')
-            label = 'as={' + label + '}' if label else ''
-            # geometry
-            color = d.get('color', '')
-            fill = d.get('fill', '')
-            shape = d.get('shape', '')
-            # style
-            style = ', '.join(filter(None, [label, color, fill, shape]))
-            style = '[' + style + ']' if style else ''
-            # pack them
-            s += '{n}{style};\n'.format(n=n, style=style)
-
-        s += '\n'
-        if nx.is_directed(self.graph):
-            line = ' -> '
         else:
-            line = ' -- '
-        for u, v, d in self.graph.edges_iter(data=True):
-            if use_label:
-                label = d.get('label', '')
-                color = d.get('color', '')
-            else:
-                label = str(d)
-                color = ''
-            if label:
-                label = '"' + label + '"\' above'
-            loop = 'loop' if u is v else ''
-            style = ', '.join(filter(None, [label, color, loop]))
-            style = ' [' + style + '] ' if style else ''
-            s += str(u) + line + style + str(v) + ';\n'
-        tikzpicture = (
-            r'\begin{{tikzpicture}}\n'
-            '\graph[{layout} layout, sibling distance=5.0cm,'
-            # 'edge quotes mid,'
-            'edges={{nodes={{ sloped, inner sep=10pt }} }},'
-            'nodes={{circle, draw}} ]{{\n'
-            '{s}'
-            '}};\n'
-            '\end{{tikzpicture}}\n').format(
-                layout=layout,
-                s=s)
-        return tikzpicture
-
-    def preamble(self, layout='layered'):
-        """Return preamble and begin/end document.
-        
-        """
-        if layout == 'layered':
-            layout_lib = 'layered'
-        elif layout == 'spring':
-            layout_lib = 'force'
-        else:
-            raise ValueError(
-                'Unknown which library contains layout: {s}'
-                        .format(s=layout))
-        document = (
-            '\\n'
-            '\documentclass{{standalone}}'
-            '\n'
-            '\usepackage{{amsmath}}\n'
-            '\usepackage{{tikz}}\n'
-            '\usetikzlibrary{{graphs,graphs.standard,'
-            'graphdrawing,quotes,shapes}}\n'
-            '\usegdlibrary{{ {layout_lib} }}\n').format(
-                layout_lib=layout_lib)
-
-        return document
-
-    def document(self, layout, use_label):
-        """Return `str` that contains a preamble and tikzpicture.
-        
-        """
-        tikz = self.dumps_tikz(self.graph, layout, use_label=use_label)
-        preamble = self.preamble(layout)
-        return (
-            '{preamble}\n'
-            r'\begin{{document}}' '\n'
-            '\n'
-            '{tikz}'
-            '\end{{document}}\n').format(
-                preamble=preamble,
-                tikz=tikz)
+            '''
+            Traverse the tree preorderly and then add 
+            EVC node to the nn.Graph() graph, but only the
+            parent of the current node and it's current
+            children
+            '''
+            if self.evc_node.root != self.evc_node:
+                '''Check if accidently we
+                    passed a root, if yes root do nothing
+                '''
+                self.graph.add_node(evc_node.parent.item)
+                self.graph.add_edge(evc_node.parent.item, node.item)
+            '''
+            Now traverse only the current node's children
+            '''
+            for node in self.evc_node:
+                self.graph.add_node(node.item)
+                for child in node.children:
+                    self.graph.add_edge(node.item, child.item)   
 
 
-    def tikz_visualize(self, path):
-        """Write TikZ picture as TeX file.
-
-        """
-        s = self.dumps_tikz(self.graph)
-        with open(path, 'w') as f:
-            f.write(s)
-
-
-    def tex_visualize(self, path, use_label=True):
-        """Write TeX document (use this as an example).
-        
-        """
-        s = self.document(self.graph, layout='layered', 
-                            use_label=use_label)
-        with open(path, 'w') as f:
-            f.write(s)
-
-    def image_visualize(self, path):
+    def image_visualize(self, path, title='EVC Graph'):
         """Visualize graph in a path
 
         """
+
+        '''
+        Setup title
+        '''
+        plt.title(title)
+        
+        '''
+        Specify hierarchical position
+        '''
+
+        pos = graphviz_layout(self.graph, prog='dot')
+        
         '''
         Use nx.draw to draw the image
         '''
-        nx.draw(self.graph)
+        nx.draw(self.graph, pos=pos, 
+                with_labels=True,
+                node_size=500, cmap=plt.cm.Blues)
+
         '''
         Save the figure to a path
         '''
         plt.savefig(path)
 
-    def image_visualize_pydot(self, path):
-        """Visualize graph in a path to a pydot file
 
-        """
+    def image_visualize_dot(self, path):
         '''
         import respected pydot - write dot from networkx
         '''
         from networkx.drawing.nx_pydot import write_dot
         '''
-        create graphviz layout
+        Adjust position for hierarchy
         '''
         pos = nx.nx_agraph.graphviz_layout(self.graph)
         '''
-        do a draw
+        draw the graph
         '''
-        nx.draw(self.graph, pos=pos)
+        nx.draw(self.graph, pos=pos,with_labels=True)
         '''
-        write it to path
+        Write to file
         '''
         write_dot(self.graph, path)
 
+    def image_visualize_custom(self, path_fname, choice='dot'):
+        """Visualize graph in a path to a dot file.
+        Choices for output: canon cmap cmapx cmapx_np dot dot_json eps fig gv 
+        imap imap_np ismap json json0 mp pic plain plain-ext pov ps ps2 svg 
+        svgz tk vml vmlz xdot xdot1.2 xdot1.4 xdot_json'
+        """
+        
+        '''
+        do a draw
+        '''
+        dot_file = to_agraph(self.graph)
+        '''
+        create graphviz layout
+        '''
+        dot_file.layout('dot')
+        '''
+        write it to path
+        '''
+        dot_file.draw(str(path_fname) + '.' +  str(choice))
+
 if __name__=="__main__":
+    from orion.core.evc.tree import TreeNode
 
     a = TreeNode("a")
     b = TreeNode("b", a)
@@ -257,4 +200,10 @@ if __name__=="__main__":
     '''
 
     test_graph = evc_graph(evc_node)
+    
+    # print nodes in graph
+    print(list(test_graph.graph.nodes))
+
+    # write to disk
     test_graph.image_visualize('./tmp/graph.png')
+    test_graph.image_visualize_dot('./tmp/graph')
