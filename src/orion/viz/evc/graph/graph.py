ec067b2db4cfde7bedac273e4d1e44562cb93e39
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-05-22 17:24:06 -0400

    fixing flake8 and pylint

--- src/orion/viz/evc/graph/graph.py
+++ src/orion/viz/evc/graph/graph.py
@@ -14,27 +14,24 @@ from __future__ import (absolute_import, division,
 
 import matplotlib.pyplot as plt
 import networkx as nx
-
-from networkx.drawing.nx_agraph import to_agraph
 from networkx.drawing.nx_agraph import graphviz_layout
+from networkx.drawing.nx_agraph import to_agraph
 
-class evc_graph(object):
+
+class EvcGraph(object):
     r"""Image graph object
-    
+
     Nodes are given as EVC nodes. Given these EVC nodes we would check
     if the node is the current root if we would want to create a the
     graph of the whole EVC tree. But if not given the root or a specific
     node, we would want to print out just the
     subgraph of the EVC tree.
-    
     """
+
     def __init__(self, evc_node):
-        """Initialize the image graph
-
-        """
-
+        """Initialize the image graph"""
         '''
-        We assume that when the object get's constructred 
+        We assume that when the object get's constructred
         an EVC node is passed through.
         '''
         self.evc_node = evc_node
@@ -49,92 +46,61 @@ class evc_graph(object):
 
         '''
         Build graph based on
-        EVC nodes. Would it better be a 
+        EVC nodes. Would it better be a
         property of the class?
         '''
-        self.build_graph(root=False)
+        self.build_graph()
 
-    def build_graph(self, root=False):
+    def build_graph(self):
         """Convert the EVC tree to the networkx graph
-        
         Algorithm:
             1. check if root is true
-                1.1 if true call root() on evc node 
+                1.1 if true call root() on evc node
                     to return root
             2. do a preordertraversal of the tree and
-               build a nn.Graph() 
-
+               build a nn.Graph()
         """
+        if self.evc_node != self.evc_node.root:
+            '''Check if accidently we
+                passed a root, if yes root do nothing
+            '''
+            self.graph.add_node(self.evc_node.parent.item,
+                                color='blue', size=500)
+            self.graph.add_edge(self.evc_node.parent.item, self.evc_node.item,
+                                color='red', weight=0.84, size=300)
 
-        if root:
-            '''
-            If we would want to choose to print the whole EVC tree
-            choose as root=True so that we would start traversing
-            the whole tree with the root.
-            '''
-            self.evc_node.root
-
-            '''
-            Traverse the tree preorderly and then add 
-            EVC node to the nn.Graph() graph
-            '''
-            for node in self.evc_node:
-                self.graph.add_node(node.item)
-                for child in node.children:
-                    self.graph.add_edge(node.item, child.item)
-        
-        else:
-            '''
-            Traverse the tree preorderly and then add 
-            EVC node to the nn.Graph() graph, but only the
-            parent of the current node and it's current
-            children
-            '''
-            if self.evc_node.root != self.evc_node:
-                '''Check if accidently we
-                    passed a root, if yes root do nothing
-                '''
-                self.graph.add_node(self.evc_node.parent.item)
-                self.graph.add_edge(self.evc_node.parent.item, self.evc_node.item)
-            '''
-            Now traverse only the current node's children
-            '''
-            for node in self.evc_node:
-                self.graph.add_node(node.item)
-                for child in node.children:
-                    self.graph.add_edge(node.item, child.item)   
-
+        '''
+        Traverse the tree preorderly and then add
+        EVC node to the nn.Graph() graph
+        '''
+        for node in self.evc_node:
+            self.graph.add_node(node.item,
+                                color='blue', size=500)
+            for child in node.children:
+                self.graph.add_edge(node.item, child.item, color='red',
+                                    weight=0.84, size=300)
 
     def image_visualize(self, path, title='EVC Graph'):
-        """Visualize graph in a path
-
-        """
-
+        """Visualize graph in a path"""
         '''
         Setup title
         '''
         plt.title(title)
-        
         '''
         Specify hierarchical position
         '''
-
         pos = graphviz_layout(self.graph, prog='dot')
-        
         '''
         Use nx.draw to draw the image
         '''
-        nx.draw(self.graph, pos=pos, 
-                with_labels=True,
-                node_size=500, cmap=plt.cm.Blues)
-
+        nx.draw(self.graph, pos=pos, with_labels=True)
         '''
         Save the figure to a path
         '''
         plt.savefig(path)
 
-
     def image_visualize_dot(self, path):
+        """Visualize image to dot format"""
         '''
         import respected pydot - write dot from networkx
         '''
@@ -154,11 +120,10 @@ class evc_graph(object):
 
     def image_visualize_custom(self, path_fname, choice='dot'):
         """Visualize graph in a path to a dot file.
-        Choices for output: canon cmap cmapx cmapx_np dot dot_json eps fig gv 
-        imap imap_np ismap json json0 mp pic plain plain-ext pov ps ps2 svg 
+        Choices for output: canon cmap cmapx cmapx_np dot dot_json eps fig v
+        imap imap_np ismap json json0 mp pic plain plain-ext pov ps ps2 svg
         svgz tk vml vmlz xdot xdot1.2 xdot1.4 xdot_json'
         """
-        
         '''
         do a draw
         '''
@@ -170,21 +135,26 @@ class evc_graph(object):
         '''
         write it to path
         '''
-        dot_file.draw(str(path_fname) + '.' +  str(choice))
+        dot_file.draw(str(path_fname) + '.' + str(choice))
 
-if __name__=="__main__":
+    def __del__(self):
+        """Destructor"""
+        del self.graph
+
+
+if __name__ == "__main__":
     from orion.core.evc.tree import TreeNode
 
-    a = TreeNode("a")
-    b = TreeNode("b", a)
-    c = TreeNode("c", a)
-    d = TreeNode("d", a)
-    e = TreeNode("e", a)
-    f = TreeNode("f", b)
-    g = TreeNode("g", b)
-    h = TreeNode("h", e)
+    A = TreeNode("a")
+    B = TreeNode("b", A)
+    C = TreeNode("c", A)
+    D = TreeNode("d", A)
+    E = TreeNode("e", A)
+    F = TreeNode("f", B)
+    G = TreeNode("g", B)
+    H = TreeNode("h", E)
 
-    '''
+    r'''
         # Gives this tree
         # a
         # |   \  \   \
@@ -193,32 +163,27 @@ if __name__=="__main__":
         # f  g        h
     '''
 
-    evc_node = a 
+    '''
+    a is root of EVC tree
+    '''
+
+    TEST_GRAPH = EvcGraph(A)
+    # print nodes in graph
+    print(list(TEST_GRAPH.graph.nodes))
+
+    # write to disk
+    TEST_GRAPH.image_visualize('./tmp/graph.png')
+    TEST_GRAPH.image_visualize_dot('./tmp/graph.dot')
 
     '''
     a is root of EVC tree
     '''
 
-    test_graph = evc_graph(evc_node)
-    
+    TEST_GRAPH2 = EvcGraph(B)
+
     # print nodes in graph
-    print(list(test_graph.graph.nodes))
+    print(list(TEST_GRAPH2.graph.nodes))
 
     # write to disk
-    test_graph.image_visualize('./tmp/graph.png')
-    test_graph.image_visualize_dot('./tmp/graph.dot')
-
-    evc_node = b
-
-    '''
-    a is root of EVC tree
-    '''
-
-    test_graph = evc_graph(evc_node)
-    
-    # print nodes in graph
-    print(list(test_graph.graph.nodes))
-
-    # write to disk
-    test_graph.image_visualize('./tmp/subgraph.png')
-    test_graph.image_visualize_dot('./tmp/subgraph.dot')
+    TEST_GRAPH2.image_visualize('./tmp/subgraph.png')
+    TEST_GRAPH2.image_visualize_dot('./tmp/subgraph.dot')
