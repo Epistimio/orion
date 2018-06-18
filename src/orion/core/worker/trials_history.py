10f9dd7790e08762be293ab2cfa78df0a344b4de
François Corneau-Tremblay <corneau90@gmail.com>
2018-06-18 13:52:38 -0400

    Fix some flake8 and pylint issues inside trials_history

--- src/orion/core/worker/trials_history.py
+++ src/orion/core/worker/trials_history.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 :mod:`orion.core.worker.trials_history` -- History of past trials
-================================================================
+=================================================================
 
 .. module:: trials_history
     :platform: Unix
@@ -10,16 +10,19 @@
 """
 
 
+# pylint:disable=protected-access,too-few-public-methods
 class TrialsHistory:
     """Maintain a list of all the last seen trials that are on different dependency paths"""
 
     def __init__(self):
+        """Create empty trials history"""
         self.parents = []
 
-    def get_most_recent_parents(self):
-        return self.parents
-
     def update_parents(self, trials):
+        """
+        Update the current parents by discarding any old ones while keeping the ones that don't
+        have children
+        """
         for trial in trials:
             self.parents = [parent for parent in self.parents if parent not in trial.parents]
         self.parents.extend([trial._id for trial in trials])
