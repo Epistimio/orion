4c86ca0d250d13f544ef36f59a016c5b039a5688
François Corneau-Tremblay <corneau90@gmail.com>
2018-06-26 15:21:35 -0400

    Add new analyses and add documentation

--- src/orion/viz/analysis/__init__.py
+++ src/orion/viz/analysis/__init__.py
@@ -8,18 +8,35 @@
    :synopsis: Containers for the analysers' output to the plotters
 """
 
+from abc import ABCMeta
 
-class Analysis(object):
+
+class Analysis(object, metaclass=ABCMeta):
     """Base class for an analysis"""
-    pass
+
+    @property
+    def value(self):
+        return None
 
 
 class SingleValueAnalysis(Analysis):
     """Analysis storing a single value"""
-
     def __init__(self, value):
         self._value = value
 
     @property
     def value(self):
         return self._value
+
+
+class TimeSeriesAnalysis(Analysis):
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, XY_dict):
+        self._value = XY_dict
