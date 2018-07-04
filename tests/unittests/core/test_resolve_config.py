e78b2e01bc27c840ad259d5a5a39c7d3db8b39fe
Reyhane Askari <r.askari.hemmat@gmail.com>
2018-07-04 11:51:29 -0400

    added test for infer_versioning_metadata

---
+++ tests/unittests/core/test_resolve_config.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`orion.core.io.resolve_config`."""
+
+import git
+import os
+import shutil
+from orion.core.cli import resolve_config
+import hashlib
+
+join = os.path.join
+
+
+def test_infer_versioning_metadata():
+    os.chdir('../')
+    try:
+        shutil.rmtree('dummy_orion')
+    except:
+        pass
+    if not os.path.exists('dummy_orion'):
+        os.makedirs('dummy_orion')
+        os.chdir('dummy_orion')
+        git.Repo.clone_from('https://github.com/ReyhaneAskari/dummy_orion.git', '.')
+
+    test_repo = git.Repo('.git')
+
+    existing_metadata = {}
+    existing_metadata['user_script'] = '.git'
+    existing_metadata = resolve_config.infer_versioning_metadata(existing_metadata)
+    assert not existing_metadata['VCS']['is_dirty']
+    assert existing_metadata['VCS']['HEAD_sha'] == 'd316303cfcd1394df861ec33658faf42083d3d55'
+    assert existing_metadata['VCS']['active_branch'] == 'master'
+    # the diff should be empty so the diff_sha should be equal to the diff sha of an empty string
+    assert existing_metadata['VCS']['diff_sha'] == hashlib.sha256(''.encode('utf-8')).hexdigest()
+
+    test_repo.create_head('feature')
+    test_repo.git.checkout('feature')
+    with open('README.md', 'w+') as f:
+        f.write('dummy content')
+    existing_metadata = resolve_config.infer_versioning_metadata(existing_metadata)
+    assert existing_metadata['VCS']['is_dirty']
+    assert existing_metadata['VCS']['active_branch'] == 'feature'
+    assert existing_metadata['VCS']['diff_sha'] != hashlib.sha256(''.encode('utf-8')).hexdigest()
+    test_repo.git.add('README.md')
+    commit = test_repo.index.commit('Added dummy_file')
+    existing_metadata = resolve_config.infer_versioning_metadata(existing_metadata)
+    assert not existing_metadata['VCS']['is_dirty']
+    assert existing_metadata['VCS']['HEAD_sha'] == commit.hexsha
+    assert existing_metadata['VCS']['diff_sha'] == hashlib.sha256(''.encode('utf-8')).hexdigest()
+
+    os.chdir('../')
+    shutil.rmtree('dummy_orion')
