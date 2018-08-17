1137d4149912c712994ce75bec5d66f1f43c0672
François Corneau-Tremblay <corneau90@gmail.com>
2018-08-17 11:38:57 -0400

    Add tests for Concept

--- tests/unittests/core/utils/concepts/concepts/src/orion/concepts/baseconceptimpl.py
+++ tests/unittests/core/utils/concepts/concepts/src/orion/concepts/baseconceptimpl.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-
+# flake8: noqa
 from orion.concepts.base import BaseConcept
 
 
 class BaseConceptImpl(BaseConcept):
+    """Define a dummy implementation of the base concept"""
 
     def __init__(self, arg=0):
+        """Initialize the dummy implementation"""
         super(BaseConceptImpl, self).__init__(arg=arg)
--- tests/unittests/core/utils/concepts/concepts/src/orion/concepts/entrypointconcept.py
+++ tests/unittests/core/utils/concepts/concepts/src/orion/concepts/entrypointconcept.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-
-
+# flake8: noqa
 from orion.concepts.base import BaseConcept
 
 
 class EntryPointConcept(BaseConcept):
+    """Define a concept that will be found using an entry point"""
 
     def __init__(self):
+        """Initialize the concept"""
         super(BaseConcept, self).__init__()
