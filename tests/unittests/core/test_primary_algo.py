1e8ac58aef0d74cda559eae42be739729061c75f
François Corneau-Tremblay <corneau90@gmail.com>
2018-08-21 13:51:11 -0400

    Change tests for new Database call

--- tests/unittests/core/test_primary_algo.py
+++ tests/unittests/core/test_primary_algo.py
@@ -12,10 +12,6 @@ def palgo(dumbalgo, space, fixed_suggestion):
     """Set up a PrimaryAlgo with dumb configuration."""
     algo_config = {'DumbAlgo': {
         'value': fixed_suggestion,
-        'subone': {'DumbAlgo': dict(
-            value=6,
-            scoring=5
-            )}
         }}
     palgo = PrimaryAlgo(space, algo_config)
 
@@ -38,15 +34,6 @@ class TestPrimaryAlgoWraps(object):
                 'judgement': None,
                 'suspend': False,
                 'done': False,
-                'subone': {
-                    'dumbalgo': {
-                        'value': 6,
-                        'scoring': 5,
-                        'judgement': None,
-                        'suspend': False,
-                        'done': False,
-                        }
-                    }
                 }
             }
 
