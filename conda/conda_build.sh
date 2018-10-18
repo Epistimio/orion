d231f46009588d9f4ff4aa6c586e5df72b6da97d
Arnaud Bergeron <abergeron@gmail.com>
2018-10-18 13:33:58 -0400

    Make sure to upload the build platform package too.

--- conda/conda_build.sh
+++ conda/conda_build.sh
@@ -16,6 +16,8 @@ conda build conda
 
 if [[ -n "${TRAVIS_TAG}" ]]
 then
-    conda convert --platform all $HOME/miniconda/conda-bld/linux-64/orion* --output-dir conda-bld/
+    mkdir -p conda-bld/linux-64
+    cp $HOME/miniconda/conda-bld/linux-64/orion* conda-bld/linux-64/
+    conda convert --platform all conda-bld/linux-64/orion* --output-dir conda-bld/
     anaconda -t $ANACONDA_TOKEN upload conda-bld/**/orion*
 fi
