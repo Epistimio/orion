536f12f485018ffbc0c5b34bb00d58176d29e35a
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-03-06 16:16:53 -0500

    Add seeding to BaseAlgorithm
    Why:
    
    Some (if not many) algorithms are random and should be _seedable_.
    
    How:
    
    Add the method `seed_rng()` which may seed the algorithm's interval
    random number generator. For flexibility, the implementation of the
    seeding mechanism is speficic to each algorithm and there is no standard
    `RNG` object made public.

---
+++ tests/unittests/algo/test_random.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`orion.algo.random`."""
+import numpy
+import pytest
+
+from orion.algo.random import Random
+from orion.algo.space import Integer, Real, Space
+
+
+@pytest.fixture()
+def space():
+    """Return an optimization space"""
+    space = Space()
+    dim1 = Integer('yolo1', 'uniform', -3, 6)
+    space.register(dim1)
+    dim2 = Real('yolo2', 'norm', 0.9)
+    space.register(dim2)
+
+    return space
+
+
+def test_seeding(space):
+    """Verify that seeding makes sampling deterministic"""
+    random_search = Random(space)
+
+    random_search.seed_rng(1)
+    a = random_search.suggest(1)[0]
+    assert not numpy.allclose(a, random_search.suggest(1)[0])
+
+    random_search.seed_rng(1)
+    assert numpy.allclose(a, random_search.suggest(1)[0])
