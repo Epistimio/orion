42fe97cf4dc83846c26a8505492867a995b601df
François Corneau-Tremblay <corneau90@gmail.com>
2019-07-11 10:48:29 -0400

    Remove pptree dependency

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -94,7 +94,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**/pptree.py']
 
 # The name of the Pygments (syntax highlighting) style to use.
 highlight_language = 'python3'
