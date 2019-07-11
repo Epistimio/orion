4f475d376bbf4b213a6910b1f2aac25915e46720
François Corneau-Tremblay <corneau90@gmail.com>
2019-07-11 10:26:28 -0400

    Fix flake8 for broken_box.py

--- tests/functional/demo/broken_box.py
+++ tests/functional/demo/broken_box.py
@@ -3,17 +3,15 @@
 """Simple one dimensional example for a possible user's script."""
 import argparse
 
-from orion.client import report_results
-
 
 def execute():
     """Execute a simple pipeline as an example."""
     # 1. Receive inputs as you want
     parser = argparse.ArgumentParser()
     parser.add_argument('-x', type=float, required=True)
-    inputs = parser.parse_args()
 
     raise RuntimeError
+
 
 if __name__ == "__main__":
     execute()
