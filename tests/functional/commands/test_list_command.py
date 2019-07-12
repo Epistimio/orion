dbdf47e2550f643eccfca50648f4049bd84f7335
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-07-11 21:47:30 -0400

    Handle --name argument properly
    Why:
    
    The argument --name was currently ignored. It should filter the name of
    the experiments

--- tests/functional/commands/test_list_command.py
+++ tests/functional/commands/test_list_command.py
@@ -90,3 +90,43 @@ def test_three_exp(capsys, three_experiments):
 
     assert captured == " test_list_double┐\n                 └test_list_double_child\n \
 test_list_single\n"
+
+
+def test_no_exp_name(three_experiments, monkeypatch, capsys):
+    """Test that nothing is printed when there are no experiments with a given name."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+    orion.core.cli.main(['list', '--name', 'I don\'t exist'])
+
+    captured = capsys.readouterr().out
+
+    assert captured == ""
+
+
+def test_exp_name(three_experiments, monkeypatch, capsys):
+    """Test that only the specified experiment is printed."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+    orion.core.cli.main(['list', '--name', 'test_list_single'])
+
+    captured = capsys.readouterr().out
+
+    assert captured == " test_list_single\n"
+
+
+def test_exp_name_with_child(three_experiments, monkeypatch, capsys):
+    """Test that only the specified experiment is printed, and with its child."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+    orion.core.cli.main(['list', '--name', 'test_list_double'])
+
+    captured = capsys.readouterr().out
+
+    assert captured == " test_list_double┐\n                 └test_list_double_child\n"
+
+
+def test_exp_name_child(three_experiments, monkeypatch, capsys):
+    """Test that only the specified child experiment is printed."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+    orion.core.cli.main(['list', '--name', 'test_list_double_child'])
+
+    captured = capsys.readouterr().out
+
+    assert captured == " test_list_double_child\n"
