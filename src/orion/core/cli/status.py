78388337137f3c4ff6e15ac97f19e60221cc171a
François Corneau-Tremblay <corneau90@gmail.com>
2019-07-16 21:09:55 -0400

    Add new `status` command (#222)

---
+++ src/orion/core/cli/status.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:mod:`orion.core.cli.status` -- Module to status experiments
+============================================================
+
+.. module:: status
+   :platform: Unix
+   :synopsis: List the trials and their statuses for experiments.
+
+"""
+import collections
+import logging
+
+import tabulate
+
+from orion.core.cli import base as cli
+from orion.core.io.database import Database
+from orion.core.io.evc_builder import EVCBuilder
+from orion.core.io.experiment_builder import ExperimentBuilder
+
+log = logging.getLogger(__name__)
+
+
+def add_subparser(parser):
+    """Add the subparser that needs to be used for this command"""
+    status_parser = parser.add_parser('status', help='status help')
+
+    cli.get_basic_args_group(status_parser)
+
+    status_parser.add_argument(
+        '-a', '--all', action="store_true",
+        help="Show all trials line by line. Otherwise, they are all aggregated by status")
+
+    status_parser.add_argument(
+        '-r', '--recursive', action="store_true",
+        help="Divide trials per experiments hierarchically. Otherwise they are all print on the \
+              same tab level.")
+
+    status_parser.set_defaults(func=main)
+
+    return status_parser
+
+
+def main(args):
+    """Fetch config and status experiments"""
+    builder = ExperimentBuilder()
+    local_config = builder.fetch_full_config(args, use_db=False)
+    builder.setup_database(local_config)
+
+    experiments = get_experiments(args)
+
+    if args.get('recursive'):
+        for exp in filter(lambda e: e.refers['parent_id'] is None, experiments):
+            print_status_recursively(exp, all_trials=args.get('all'))
+    else:
+        for exp in experiments:
+            print_status(exp, all_trials=args.get('all'))
+
+
+def get_experiments(args):
+    """Return the different experiments.
+
+    Parameters
+    ----------
+    args: dict
+        Commandline arguments.
+
+    """
+    projection = {'name': 1}
+
+    query = {'name': args['name']} if args.get('name') else {}
+    experiments = Database().read("experiments", query, projection)
+
+    return [EVCBuilder().build_from({'name': exp['name']}) for exp in experiments]
+
+
+def print_status_recursively(exp, depth=0, **kwargs):
+    """Print the status recursively of the children of the current experiment.
+
+    Parameters
+    ----------
+    exp: `orion.core.worker.Experiment`
+        The current experiment to print.
+    depth: int
+        The current depth of the tree.
+
+    """
+    print_status(exp, offset=depth * 2, **kwargs)
+
+    for child in exp.node.children:
+        print_status_recursively(child.item, depth + 1, **kwargs)
+
+
+def print_status(exp, offset=0, all_trials=False):
+    """Print the status of the current experiment.
+
+    Parameters
+    ----------
+    offset: int, optional
+        The number of tabs to the right this experiment is.
+    all_trials: bool, optional
+        Print all trials individually
+
+    """
+    if all_trials:
+        print_all_trials(exp, offset=offset)
+    else:
+        print_summary(exp, offset=offset)
+
+
+def print_summary(exp, offset=0):
+    """Print a summary of the current experiment.
+
+    Parameters
+    ----------
+    offset: int, optional
+        The number of tabs to the right this experiment is.
+
+    """
+    status_dict = collections.defaultdict(list)
+    name = exp.name
+    trials = exp.fetch_trials({})
+
+    for trial in trials:
+        status_dict[trial.status].append(trial)
+
+    print(" " * offset, name, sep="")
+    print(" " * offset, "=" * len(name), sep="")
+
+    headers = ['status', 'quantity']
+
+    lines = []
+    for status, trials in sorted(status_dict.items()):
+        line = [status, len(trials)]
+
+        if trials[0].objective:
+            headers.append('min {}'.format(trials[0].objective.name))
+            line.append(min(trial.objective.value for trial in trials))
+
+        lines.append(line)
+
+    if trials:
+        grid = tabulate.tabulate(lines, headers=headers)
+        tab = " " * offset
+        print(tab + ("\n" + tab).join(grid.split("\n")))
+    else:
+        print(" " * offset, 'empty', sep="")
+
+    print("\n")
+
+
+def print_all_trials(exp, offset=0):
+    """Print all trials of the current experiment individually.
+
+    Parameters
+    ----------
+    offset: int, optional
+        The number of tabs to the right this experiment is.
+
+    """
+    name = exp.name
+    trials = exp.fetch_trials({})
+
+    print(" " * offset, name, sep="")
+    print(" " * offset, "=" * len(name), sep="")
+    headers = ['id', 'status', 'best objective']
+    lines = []
+
+    for trial in sorted(trials, key=lambda t: t.status):
+        line = [trial.id, trial.status]
+
+        if trial.objective:
+            headers[-1] = 'min {}'.format(trial.objective.name)
+            line.append(trial.objective.value)
+
+        lines.append(line)
+
+    grid = tabulate.tabulate(lines, headers=headers)
+    tab = " " * offset
+    print(tab + ("\n" + tab).join(grid.split("\n")))
+
+    print("\n")
---
+++ tests/functional/commands/test_status_command.py
@@ -0,0 +1,742 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Perform a functional test of the status command."""
+import os
+
+import orion.core.cli
+
+
+def test_no_experiments(clean_db, monkeypatch, capsys):
+    """Test status with no experiments."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+    orion.core.cli.main(['status'])
+
+    captured = capsys.readouterr().out
+
+    assert captured == ""
+
+
+def test_experiment_without_trials_wout_ar(clean_db, one_experiment, capsys):
+    """Test status with only one experiment and no trials."""
+    orion.core.cli.main(['status'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_single_exp
+===============
+empty
+
+
+"""
+    assert captured == expected
+
+
+def test_experiment_wout_success_wout_ar(clean_db, single_without_success, capsys):
+    """Test status with only one experiment and no successful trial."""
+    orion.core.cli.main(['status'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_single_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+    assert captured == expected
+
+
+def test_two_w_trials_wout_ar(clean_db, unrelated_with_trials, capsys):
+    """Test two unrelated experiments, with all types of trials."""
+    orion.core.cli.main(['status'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+test_single_exp
+===============
+status         quantity    min obj
+-----------  ----------  ---------
+broken                1
+completed             1          0
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_fam_w_trials_wout_ar(clean_db, family_with_trials, capsys):
+    """Test two related experiments, with all types of trials."""
+    orion.core.cli.main(['status'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+test_double_exp_child
+=====================
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_one_wout_trials_w_a_wout_r(clean_db, one_experiment, capsys):
+    """Test experiments, without trials, with --all."""
+    orion.core.cli.main(['status', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_single_exp
+===============
+id    status    best objective
+----  --------  ----------------
+
+
+"""
+
+    assert captured == expected
+
+
+def test_one_w_trials_w_a_wout_r(clean_db, single_with_trials, capsys):
+    """Test experiment, with all trials, with --all."""
+    orion.core.cli.main(['status', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_single_exp
+===============
+id                                status         min obj
+--------------------------------  -----------  ---------
+78d300480fc80ec5f52807fe97f65dd7  broken
+7e8eade99d5fb1aa59a1985e614732bc  completed            0
+ec6ee7892275400a9acbf4f4d5cd530d  interrupted
+507496236ff94d0f3ad332949dfea484  new
+caf6afc856536f6d061676e63d14c948  reserved
+2b5059fa8fdcdc01f769c31e63d93f24  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_one_wout_success_w_a_wout_r(clean_db, single_without_success, capsys):
+    """Test experiment, without success, with --all."""
+    orion.core.cli.main(['status', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_single_exp
+===============
+id                                status
+--------------------------------  -----------
+78d300480fc80ec5f52807fe97f65dd7  broken
+ec6ee7892275400a9acbf4f4d5cd530d  interrupted
+507496236ff94d0f3ad332949dfea484  new
+caf6afc856536f6d061676e63d14c948  reserved
+2b5059fa8fdcdc01f769c31e63d93f24  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_unrelated_w_a_wout_r(clean_db, unrelated_with_trials, capsys):
+    """Test two unrelated experiments with --all."""
+    orion.core.cli.main(['status', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+test_single_exp
+===============
+id                                status         min obj
+--------------------------------  -----------  ---------
+78d300480fc80ec5f52807fe97f65dd7  broken
+7e8eade99d5fb1aa59a1985e614732bc  completed            0
+ec6ee7892275400a9acbf4f4d5cd530d  interrupted
+507496236ff94d0f3ad332949dfea484  new
+caf6afc856536f6d061676e63d14c948  reserved
+2b5059fa8fdcdc01f769c31e63d93f24  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_related_w_a_wout_r(clean_db, family_with_trials, capsys):
+    """Test two related experiments with --all."""
+    orion.core.cli.main(['status', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+test_double_exp_child
+=====================
+id                                status
+--------------------------------  -----------
+b55c5a82050dc30a6b0c9614b1eb05e5  broken
+649e09b84128c2f8821b9225ebcc139b  completed
+bac4e23fae8fe316d6f763ac901569af  interrupted
+5f4a9c92b8f7c26654b5b37ecd3d5d32  new
+c2df0712319b5e91c1b4176e961a07a7  reserved
+382400953aa6e8769e11aceae9be09d7  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_unrelated_w_r_wout_a(clean_db, unrelated_with_trials, capsys):
+    """Test two unrelated experiments with --recursive."""
+    orion.core.cli.main(['status', '--recursive'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+test_single_exp
+===============
+status         quantity    min obj
+-----------  ----------  ---------
+broken                1
+completed             1          0
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_related_w_r_wout_a(clean_db, family_with_trials, capsys):
+    """Test two related experiments with --recursive."""
+    orion.core.cli.main(['status', '--recursive'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+  test_double_exp_child
+  =====================
+  status         quantity
+  -----------  ----------
+  broken                1
+  completed             1
+  interrupted           1
+  new                   1
+  reserved              1
+  suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_unrelated_w_r_wout_a(clean_db, three_experiments_with_trials, capsys):
+    """Test three unrelated experiments with --recursive."""
+    orion.core.cli.main(['status', '--recursive'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+  test_double_exp_child
+  =====================
+  status         quantity
+  -----------  ----------
+  broken                1
+  completed             1
+  interrupted           1
+  new                   1
+  reserved              1
+  suspended             1
+
+
+test_single_exp
+===============
+status         quantity    min obj
+-----------  ----------  ---------
+broken                1
+completed             1          0
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_related_w_r_wout_a(clean_db, three_family_with_trials, capsys):
+    """Test three related experiments with --recursive."""
+    orion.core.cli.main(['status', '--recursive'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+  test_double_exp_child
+  =====================
+  status         quantity
+  -----------  ----------
+  broken                1
+  completed             1
+  interrupted           1
+  new                   1
+  reserved              1
+  suspended             1
+
+
+  test_double_exp_child2
+  ======================
+  status         quantity
+  -----------  ----------
+  broken                1
+  completed             1
+  interrupted           1
+  new                   1
+  reserved              1
+  suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_related_branch_w_r_wout_a(clean_db, three_family_branch_with_trials, capsys):
+    """Test three related experiments with --recursive."""
+    orion.core.cli.main(['status', '--recursive'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+  test_double_exp_child
+  =====================
+  status         quantity
+  -----------  ----------
+  broken                1
+  completed             1
+  interrupted           1
+  new                   1
+  reserved              1
+  suspended             1
+
+
+    test_double_exp_grand_child
+    ===========================
+    status         quantity
+    -----------  ----------
+    broken                1
+    completed             1
+    interrupted           1
+    new                   1
+    reserved              1
+    suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_unrelated_w_ar(clean_db, unrelated_with_trials, capsys):
+    """Test two unrelated experiments with --recursive and --all."""
+    orion.core.cli.main(['status', '--recursive', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+test_single_exp
+===============
+id                                status         min obj
+--------------------------------  -----------  ---------
+78d300480fc80ec5f52807fe97f65dd7  broken
+7e8eade99d5fb1aa59a1985e614732bc  completed            0
+ec6ee7892275400a9acbf4f4d5cd530d  interrupted
+507496236ff94d0f3ad332949dfea484  new
+caf6afc856536f6d061676e63d14c948  reserved
+2b5059fa8fdcdc01f769c31e63d93f24  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_two_related_w_ar(clean_db, family_with_trials, capsys):
+    """Test two related experiments with --recursive and --all."""
+    orion.core.cli.main(['status', '--recursive', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+  test_double_exp_child
+  =====================
+  id                                status
+  --------------------------------  -----------
+  b55c5a82050dc30a6b0c9614b1eb05e5  broken
+  649e09b84128c2f8821b9225ebcc139b  completed
+  bac4e23fae8fe316d6f763ac901569af  interrupted
+  5f4a9c92b8f7c26654b5b37ecd3d5d32  new
+  c2df0712319b5e91c1b4176e961a07a7  reserved
+  382400953aa6e8769e11aceae9be09d7  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_unrelated_w_ar(clean_db, three_experiments_with_trials, capsys):
+    """Test three unrelated experiments with --recursive and --all."""
+    orion.core.cli.main(['status', '--recursive', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+  test_double_exp_child
+  =====================
+  id                                status
+  --------------------------------  -----------
+  b55c5a82050dc30a6b0c9614b1eb05e5  broken
+  649e09b84128c2f8821b9225ebcc139b  completed
+  bac4e23fae8fe316d6f763ac901569af  interrupted
+  5f4a9c92b8f7c26654b5b37ecd3d5d32  new
+  c2df0712319b5e91c1b4176e961a07a7  reserved
+  382400953aa6e8769e11aceae9be09d7  suspended
+
+
+test_single_exp
+===============
+id                                status         min obj
+--------------------------------  -----------  ---------
+78d300480fc80ec5f52807fe97f65dd7  broken
+7e8eade99d5fb1aa59a1985e614732bc  completed            0
+ec6ee7892275400a9acbf4f4d5cd530d  interrupted
+507496236ff94d0f3ad332949dfea484  new
+caf6afc856536f6d061676e63d14c948  reserved
+2b5059fa8fdcdc01f769c31e63d93f24  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_related_w_ar(clean_db, three_family_with_trials, capsys):
+    """Test three related experiments with --recursive and --all."""
+    orion.core.cli.main(['status', '--recursive', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+  test_double_exp_child
+  =====================
+  id                                status
+  --------------------------------  -----------
+  b55c5a82050dc30a6b0c9614b1eb05e5  broken
+  649e09b84128c2f8821b9225ebcc139b  completed
+  bac4e23fae8fe316d6f763ac901569af  interrupted
+  5f4a9c92b8f7c26654b5b37ecd3d5d32  new
+  c2df0712319b5e91c1b4176e961a07a7  reserved
+  382400953aa6e8769e11aceae9be09d7  suspended
+
+
+  test_double_exp_child2
+  ======================
+  id                                status
+  --------------------------------  -----------
+  d0f4aa931345bfd864201b7dd93ae667  broken
+  5005c35be98025a24731d7dfdf4423de  completed
+  c9fa9f0682a370396c8c4265c4e775dd  interrupted
+  3d8163138be100e37f1656b7b591179e  new
+  790d3c4c965e0d91ada9cbdaebe220cf  reserved
+  6efdb99952d5f80f55adbba9c61dc288  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_three_related_branch_w_ar(clean_db, three_family_branch_with_trials, capsys):
+    """Test three related experiments in a branch with --recursive and --all."""
+    orion.core.cli.main(['status', '--recursive', '--all'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+id                                status
+--------------------------------  -----------
+a8f8122af9e5162e1e2328fdd5dd75db  broken
+ab82b1fa316de5accb4306656caa07d0  completed
+c187684f7c7d9832ba953f246900462d  interrupted
+1497d4f27622520439c4bc132c6046b1  new
+bd0999e1a3b00bf8658303b14867b30e  reserved
+b9f1506db880645a25ad9b5d2cfa0f37  suspended
+
+
+  test_double_exp_child
+  =====================
+  id                                status
+  --------------------------------  -----------
+  b55c5a82050dc30a6b0c9614b1eb05e5  broken
+  649e09b84128c2f8821b9225ebcc139b  completed
+  bac4e23fae8fe316d6f763ac901569af  interrupted
+  5f4a9c92b8f7c26654b5b37ecd3d5d32  new
+  c2df0712319b5e91c1b4176e961a07a7  reserved
+  382400953aa6e8769e11aceae9be09d7  suspended
+
+
+    test_double_exp_grand_child
+    ===========================
+    id                                status
+    --------------------------------  -----------
+    994602c021c470989d6f392b06cb37dd  broken
+    24c228352de31010d8d3bf253604a82d  completed
+    a3c8a1f4c80c094754c7217a83aae5e2  interrupted
+    d667f5d719ddaa4e1da2fbe568e11e46  new
+    a40748e487605df3ed04a5ac7154d4f6  reserved
+    229622a6d7132c311b7d4c57a08ecf08  suspended
+
+
+"""
+
+    assert captured == expected
+
+
+def test_experiment_wout_child_w_name(clean_db, unrelated_with_trials, capsys):
+    """Test status with the name argument and no child."""
+    orion.core.cli.main(['status', '--name', 'test_single_exp'])
+
+    captured = capsys.readouterr().out
+
+    expected = """test_single_exp
+===============
+status         quantity    min obj
+-----------  ----------  ---------
+broken                1
+completed             1          0
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
+
+
+def test_experiment_w_child_w_name(clean_db, three_experiments_with_trials, capsys):
+    """Test status with the name argument and one child."""
+    orion.core.cli.main(['status', '--name', 'test_double_exp'])
+
+    captured = capsys.readouterr().out
+
+    expected = """\
+test_double_exp
+===============
+status         quantity
+-----------  ----------
+broken                1
+completed             1
+interrupted           1
+new                   1
+reserved              1
+suspended             1
+
+
+"""
+
+    assert captured == expected
