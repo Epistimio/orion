7bb4bd7219c01ae44a9de6db84dc6fdbe50cc6d0
François Corneau-Tremblay <corneau90@gmail.com>
2019-07-30 14:38:09 -0400

    Add tests for Experiment version

--- src/orion/core/worker/experiment.py
+++ src/orion/core/worker/experiment.py
@@ -125,6 +125,7 @@ class Experiment:
         self._storage = get_storage()
 
         config = self._storage.fetch_experiments({'name': name, 'metadata.user': user})
+        print(config)
 
         if config:
             log.debug("Found existing experiment, %s, under user, %s, registered in database.",
