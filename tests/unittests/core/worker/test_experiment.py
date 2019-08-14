b2205b10f7b23c9b02c474fd0e451ebc61902213
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-08-14 11:01:20 -0400

    update messages

--- tests/unittests/core/worker/test_experiment.py
+++ tests/unittests/core/worker/test_experiment.py
@@ -65,17 +65,13 @@ def test_change_status_success(exp_config_file):
     def check_status_change(new_status):
         with OrionState(from_yaml=exp_config_file) as cfg:
             trial = cfg.get_trial(0)
-            assert trial is not None, 'Was not able to reserve trials for test'
+            assert trial is not None, 'was not able to retrieve trial for test'
 
-            try:
-                get_storage().set_trial_status(trial, status=new_status)
-                assert trial.status == new_status, 'Trial status should have been updated locally'
+            get_storage().set_trial_status(trial, status=new_status)
+            assert trial.status == new_status, 'Trial status should have been updated locally'
 
-                trial = get_storage().get_trial(trial)
-                assert trial.status == new_status, 'Trial status should have been updated in the storage'
-
-            except FailedUpdate:
-                pass
+            trial = get_storage().get_trial(trial)
+            assert trial.status == new_status, 'Trial status should have been updated in the storage'
 
     check_status_change('completed')
     check_status_change('broken')
@@ -89,7 +85,7 @@ def test_change_status_failed_update(exp_config_file):
     """Successfully find new trials in db and reserve one at 'random'."""
     with OrionState(from_yaml=exp_config_file) as cfg:
         trial = cfg.get_trial(0)
-        assert trial is not None, 'Was not able to reserve trials for test'
+        assert trial is not None, 'Was not able to retrieve trial for test'
 
         try:
             trial.status = 'completed'
