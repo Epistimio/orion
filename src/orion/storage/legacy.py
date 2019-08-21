e8469088ecd67dcec7379ddf5fbc6e5f8979fe16
Setepenre <pierre.delaunay.tr@gmail.com>
2019-08-21 13:02:29 -0400

    Update src/orion/storage/legacy.py
    Co-Authored-By: Xavier Bouthillier <xavier.bouthillier@gmail.com>

--- src/orion/storage/legacy.py
+++ src/orion/storage/legacy.py
@@ -14,7 +14,7 @@ import logging
 from orion.core.io.convert import JSONConverter
 from orion.core.io.database import Database
 from orion.core.worker.trial import Trial
-from orion.storage.base import BaseStorageProtocol, FailedUpdate, UnderdefinedCall
+from orion.storage.base import BaseStorageProtocol, FailedUpdate, UndefinedCall
 
 log = logging.getLogger(__name__)
 
