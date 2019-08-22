0aaa48ac504e831cb9913a1e44f87d6782b7c98d
Setepenre <pierre.delaunay.tr@gmail.com>
2019-08-22 11:29:53 -0400

    Update tests/unittests/storage/test_storage.py
    Co-Authored-By: Xavier Bouthillier <xavier.bouthillier@gmail.com>

--- tests/unittests/storage/test_storage.py
+++ tests/unittests/storage/test_storage.py
@@ -468,5 +468,5 @@ class TestStorage:
                 trial3 = storage.fetch_trial_by_status(status='completed')
                 storage.update_heartbeat(trial3)
 
-                assert trial3.heartveat is None, \
+                assert trial3.heartbeat is None, \
                     'Legacy does not update trials with a status different from reserved'
