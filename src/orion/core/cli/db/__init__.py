025b3043475f0afd906fe90b823074713ccfd3df
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-10-08 11:01:58 -0400

    Turn test-db into db test
    Why:
    
    We will shortly add more database related commands such as `remove` and
    `upgrade`. They should all be grouped within `orion db`.

---
+++ src/orion/core/cli/db/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:mod:`orion.core.cli.db` -- Functions that define console scripts for database
+==============================================================================
+
+.. module:: cli
+   :platform: Unix
+   :synopsis: Commands to maintain database
+
+"""
