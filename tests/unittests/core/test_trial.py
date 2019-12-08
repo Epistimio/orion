78472cc134146a10b4aacfe99531c71dec38dcd8
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-12-07 21:26:09 -0500

    -

--- tests/unittests/core/test_trial.py
+++ tests/unittests/core/test_trial.py
@@ -211,8 +211,10 @@ class TestTrial(object):
     def test_params_repr_property(self, exp_config):
         """Check property `Trial.params_repr`."""
         t = Trial(**exp_config[1][1])
-        assert Trial.format_params(t) == "/decoding_layer:lstm_with_attention,/encoding_layer:gru"
-        assert Trial.format_params(t, sep='\n') == "/decoding_layer:lstm_with_attention\n/encoding_layer:gru"
+        assert Trial.format_params(t) == \
+               "/decoding_layer:lstm_with_attention,/encoding_layer:gru"
+        assert Trial.format_params(t, sep='\n') == \
+               "/decoding_layer:lstm_with_attention\n/encoding_layer:gru"
 
         t = Trial()
         assert t.params_repr() == ""
