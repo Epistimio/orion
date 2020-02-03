5e41b7ac10bd3fa7de7ea10ebd064c3f4a23b500
Thomsch <thomas.schweizer@mila.quebec>
2020-02-03 12:26:57 -0500

    Switched to iris and a relevant classifier
    Since this is a tutorial, we need to have a significant
    difference between using the model with and without
    hyper-parameter optimisation. The previous model gave a result
    that was too good, making hyper-parameter irrelevant.

--- examples/sklearn-iris/main.py
+++ examples/sklearn-iris/main.py
@@ -1,12 +1,12 @@
-from sklearn.datasets import load_breast_cancer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import balanced_accuracy_score
+from sklearn.model_selection import train_test_split
 
-X, y = load_breast_cancer(return_X_y=True)
+X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-clf = RandomForestClassifier(random_state=42)
+clf = SGDClassifier(random_state=42)
 clf.fit(X_train,y_train)
 
 y_pred = clf.predict(X_test)
