a73382a3de984f9de51ae54f0b037a68a9f2c1f6
Thomsch <thomas.schweizer@mila.quebec>
2020-02-04 17:32:05 -0500

    Added epsilon hyperparam as commandline argument to control huber loss

--- examples/sklearn-iris/main.py
+++ examples/sklearn-iris/main.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python/
+#!/usr/bin/env python
+import sys
 
 from sklearn.datasets import load_iris
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 
+hyper_epsilon = sys.argv[1]
+print("Epsilon is {}".format(hyper_epsilon))
+
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-clf = SGDClassifier(random_state=42)
-clf.fit(X_train,y_train)
+clf = SGDClassifier(random_state=42, loss='huber', epsilon=float(hyper_epsilon))
+clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 accuracy = balanced_accuracy_score(y_test, y_pred)
 
-print(accuracy)
+print("Accuracy is {}".format(accuracy))
