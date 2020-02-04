3f5a0f5157b41135035680d213f43ace8aca16ec
Thomsch <thomas.schweizer@mila.quebec>
2020-02-04 17:37:20 -0500

    Converted script to be compatible with Orion

--- examples/sklearn-iris/main.py
+++ examples/sklearn-iris/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 
+from orion.client import report_results
 from sklearn.datasets import load_iris
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import balanced_accuracy_score
@@ -19,3 +20,8 @@ y_pred = clf.predict(X_test)
 accuracy = balanced_accuracy_score(y_test, y_pred)
 
 print("Accuracy is {}".format(accuracy))
+
+report_results([dict(
+    name='accuracy',
+    type='objective',
+    value=1 - accuracy)])
