c555f875b09b5e5c769d59812ed302946f2804e6
Thomsch <thomas.schweizer@mila.quebec>
2020-02-05 15:49:01 -0500

    Formatting

--- examples/sklearn-iris/analysis.py
+++ examples/sklearn-iris/analysis.py
@@ -1,11 +1,9 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-
 from orion.client import create_experiment
 
 experiment = create_experiment(name="sklearn-tutorial")
-
 data = [(1 - trial.objective.value, trial.params['/_pos_0']) for trial in experiment.fetch_trials_by_status('completed')]
 
 df = pd.DataFrame(data, columns=['accuracy', 'epsilon']).sort_values('accuracy')
@@ -14,5 +12,6 @@ sns.set_style("whitegrid")
 ax = sns.scatterplot(x='epsilon', y='accuracy', data=df, color='black', alpha=0.3)
 ax.set_xscale('log')
 ax.set_xticks([0.00001, 0.0001, 0.001, 0.01, 0.1, 1])
+
 plt.savefig("hyperparameter-optimization.pdf", format='pdf')
 plt.show()
