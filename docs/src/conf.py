7bff040d7d3d677030bd61e4d595f4fa34faa227
Thomas Schweizer <thomas.schweizer@mila.quebec>
2020-08-20 15:34:57 -0400

    Sort modules and extensions alphabetically
    Unsorted list of extensions and modules makes it hard to maintain and merge back when concurrent changes are made.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -40,21 +40,17 @@ import orion.core as orion  # noqa
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.autosummary',
+extensions = [  # Extensions must be sorted alphabetically to ease maintenance and merges.
     'numpydoc',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
 ]
 
 # General information about the project.
