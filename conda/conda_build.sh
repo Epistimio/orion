51928616d6dbbd01d1ec9904d3a1f5bcec89e9fb
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-11-06 12:17:55 -0500

    Refactor conda for branch CI vs stable CI

--- conda/conda_build.sh
+++ conda/conda_build.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-set -e
-set -x
-
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-
-pip uninstall -y setuptools
-conda install -c anaconda setuptools
-
-conda update -q conda
-conda info -a
-conda install conda-build anaconda-client
-
-conda build conda --python 3.6
-conda build conda --python 3.6
-conda build conda --python 3.7
-conda build conda --python 3.8
+./conda/conda_test.sh
 
 if [[ -n "${TRAVIS_TAG}" ]]
 then
