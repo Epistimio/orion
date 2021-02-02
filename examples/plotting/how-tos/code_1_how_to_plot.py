8feede9bf685b20d43e6927b49eb0e19895d4829
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2021-02-02 15:22:55 -0500

    Revert example now that storage is fixed

--- examples/plotting/how-tos/code_1_how_to_plot.py
+++ examples/plotting/how-tos/code_1_how_to_plot.py
@@ -15,16 +15,7 @@ REST server
 from orion.client import get_experiment
 
 # Specify the database where the experiments are stored. We use a local PickleDB here.
-import os
-
 storage = dict(type="legacy", database=dict(type="pickleddb", host="../database.pkl"))
-
-from orion.storage.base import get_storage, setup_storage
-
-setup_storage(storage)
-s = get_storage()
-exp_names = sorted([e["name"] for e in s.fetch_experiments({})])
-raise RuntimeError(f"{os.getcwd()}\n{s._db.host}\n{exp_names}")
 
 # Load the data for the specified experiment
 experiment = get_experiment("lateral-view-multitask3", storage=storage)
