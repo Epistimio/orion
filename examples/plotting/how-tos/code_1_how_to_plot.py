d37404972e799405a42dbfbbdf0ccb0a29eb6556
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2021-02-02 14:54:48 -0500

    Trying to figure out what cwd is on rtd

--- examples/plotting/how-tos/code_1_how_to_plot.py
+++ examples/plotting/how-tos/code_1_how_to_plot.py
@@ -15,7 +15,10 @@ REST server
 from orion.client import get_experiment
 
 # Specify the database where the experiments are stored. We use a local PickleDB here.
-storage = dict(type="legacy", database=dict(type="pickleddb", host="./database.pkl"))
+import os
+
+raise RuntimeError(os.getcwd())
+storage = dict(type="legacy", database=dict(type="pickleddb", host="../database.pkl"))
 
 # Load the data for the specified experiment
 experiment = get_experiment("lateral-view-multitask3", storage=storage)
