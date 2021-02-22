9e54c108bd117107717ac4d97888754ce4f2b928
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2021-02-22 13:22:27 -0500

    Complete how to plot doc

--- examples/plotting/how-tos/code_1_how_to_plot.py
+++ examples/plotting/how-tos/code_1_how_to_plot.py
@@ -3,13 +3,19 @@
 How to plot.
 ============
 
-How to plot...
+There exist multiple ways to plot using Oríon. We will start with the most convenient one, the
+experiment object plot accessor.
 
-plot.<method>
+``experiment.plot`` accessor
+----------------------------
 
-exp.plot.<method>
+.. note::
 
-REST server
+    It only supports single experiment plots. It does not support ``regrets`` and ``averages``.
+
+It is possible to render most plots directly with 
+the :py:class:`ExperimentClient <orion.client.experiment.ExperimentClient>` object.
+The accessor ``ExperimentClient.plot`` can be used to plot the results of the experiment.
 
 """
 from orion.client import get_experiment
@@ -19,13 +25,70 @@ storage = dict(type="legacy", database=dict(type="pickleddb", host="../../db.pkl
 
 # Load the data for the specified experiment
 experiment = get_experiment("2-dim-exp", storage=storage)
-experiment.plot.regret()
+fig = experiment.plot.regret()
+fig.show()
 
 #%%
-# Or with plotting module
+# ``plotting`` module
+# -------------------
+#
+# The plotting module can used as well to plot experiments.
 
 import orion.plotting.base as plot
 
-plot.regret(experiment)
+fig = plot.regret(experiment)
+fig.show()
+
+#%%
+# The advantage of the plotting module is that it can render plots using
+# multiple examples as shown in the example below.
+#
+# .. todo::
+#
+#     Add example with regrets using multiple experiments from benchmark experiment.
+#
+
+fig = plot.regret(experiment)
+fig.show()
 
 # sphinx_gallery_thumbnail_path = '_static/paint.png'
+
+#%%
+# Command line
+# ------------
+#
+# .. note::
+#
+#     The plotting command line utility does not support arguments for now. Contributions are
+#     welcome! :)
+#
+# .. note::
+#
+#     Only supports single experiment plots. It does not support ``regrets`` and ``averages``.
+#
+# ``orion plot --name <exp> <plot type>``
+#
+# ::
+#
+#     orion plot --name 2-dim-exp regret
+#
+# .. TODO::
+#
+#     Complete the plot command line
+#
+# .. TODO::
+#
+#     Add documentation of the commmand line
+
+#%%
+#
+# Web API
+# -------
+#
+# .. note::
+#
+#     Only supports single experiment plots. It does not support ``regrets`` and ``averages``.
+#
+# The :ref:`web-api` supports queries for single experiment plots. See documentation for
+# all queries. The JSON output is generated automatically according to the
+# `Plotly.js schema reference <https://plotly.com/python/reference/index/>`_.
