9e815a1cc34655ab3c65acfc7b574b7e585fd16d
Guillaume Alain <gyomalin@gmail.com>
2021-03-31 17:09:21 -0400

    seems to me like everything is working, but no tests written yet

--- src/orion/core/cli/plot.py
+++ src/orion/core/cli/plot.py
@@ -31,13 +31,10 @@ def add_subparser(parser):
     cli.get_basic_args_group(plot_parser)
 
     plot_parser.add_argument(
-        "-r",
-        "--resource",
+        "kind",
         type=str,
-        default="lpi",
-        help="resource to plot;"
+        help="kind of plot to generate. "
         " Pick one among ['lpi', 'partial_dependencies', 'parallel_coordinates', 'regret']"
-        " (default: lpi)",
     )
 
     plot_parser.add_argument(
@@ -45,27 +42,28 @@ def add_subparser(parser):
         "--type",
         type=str,
         default="png",
-        help="type of plot to return;"
-        " Pick one among ['png', 'jpeg', 'json']"
+        help="type of plot to return. "
+        " Pick one among ['png', 'jpg', 'jpeg', 'webp', 'svg', 'pdf', 'html', 'json']"
         " (default: png)",
     )
 
     plot_parser.add_argument(
         "-o",
-        "--output_path",
+        "--output",
         type=str,
         default="",
-        help="path where plot is saved;"
+        help="path where plot is saved. "
         " Will override the `type` argument."
-        " (default is experiment name if applicable)"
+        " (default is {exp.name}_{kind}.{type})"
     )
 
     plot_parser.add_argument(
-        "--dpi",
-        type=int,
-        default=150,
-        help="dpi when plotting png;"
-        " (default: 150)",
+        "--scale",
+        type=float,
+        default=None,
+        help="more pixels, but same proportions of the plot. "
+        " Reference is 1.0."
+        " Overrides value of 'scale' in plotly.io.write_image."
     )
 
     plot_parser.set_defaults(func=main)
@@ -86,8 +84,8 @@ def main(args):
     
     experiment = experiment_builder.build_from_args(args)
 
-    assert args['resource'] in ['lpi', 'partial_dependencies', 'parallel_coordinates', 'regret']
-    func_plotting = getattr(orion.plotting.base, args['resource'])
+    assert args['kind'] in ['lpi', 'partial_dependencies', 'parallel_coordinates', 'regret']
+    func_plotting = getattr(orion.plotting.base, args['kind'])
 
     output_plot = func_plotting(experiment)
 
@@ -96,26 +94,31 @@ def main(args):
     # 'html' handled by fig.write_html
     # 'json' handled by just writing out the file
 
-    if args['output_path']:
-        # `output_path` was given, and we'll override `type`
-        args['type'] = args['output_path'].split(".")[-1]
+    if args['output']:
+        # `output` was given, and we'll override `type`
+        args['type'] = args['output'].split(".")[-1]
         assert args['type'] in valid_types, (
-            "Overriding the `type` field with something from `output_path`, "
+            "Overriding the `type` field with something from `output`, "
             f"but we got the invalid value : {args['type']} .")
     else:
-        # `type` was given, and we'll pick a `output_path` based on the name
+        # `type` was given, and we'll pick a `output` based on the name
         assert args['type'] in valid_types, (
             f"Invalid `type` {args['type']} .")
-        assert args["name"], "No name found in arguments."
-        args['output_path'] = args['name'] + "." + args['type']
+        # Using `experiment.name` instead of args['name'] because it leaves
+        # orion the possibility of having inferred the name in other ways.
+        args['output'] = f"{experiment.name}_{args['kind']}.{args['type']}"
 
     if args['type'] in ['png', 'jpg', 'jpeg', 'webp', 'svg', 'pdf']:
-        # TODO : Use dpi in png.
-        output_plot.write_image(args['output_path'])
+        # a way to increase pixel counts without affecting the proportions
+        kwargs = {}
+        if args['scale'] is not None:
+            kwargs['scale'] = args['scale']
+
+        output_plot.write_image(args['output'], **kwargs)
     elif args['type'] in ['html']:
-        output_plot.write_html(args['output_path'])
+        output_plot.write_html(args['output'])
     elif args['type'] in ['json']:
-        with open(args['output_path'], "w") as f_out:
+        with open(args['output'], "w") as f_out:
             # Note that this is the content of the "body" in the WebApi.
             f_out.write(output_plot.to_json())
     else:
