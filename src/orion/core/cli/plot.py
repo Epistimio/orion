85b563a5dc2cce02dc1a7e074de8fed2dec8a41a
Guillaume Alain <gyomalin@gmail.com>
2021-04-01 13:46:11 -0400

    Update src/orion/core/cli/plot.py
    Co-authored-by: Xavier Bouthillier <xavier.bouthillier@gmail.com>

--- src/orion/core/cli/plot.py
+++ src/orion/core/cli/plot.py
@@ -82,7 +82,7 @@ def main(args):
     experiment = experiment_builder.build_from_args(args)
 
     func_plotting = getattr(orion.plotting.base, args['kind'])
-    output_plot = func_plotting(experiment)
+    output_plot = experiment.plot(kind=args['kind'])
 
     valid_types = ['png', 'jpg', 'jpeg', 'webp', 'svg', 'pdf', 'html', 'json']
     # 'png', 'jpg', 'jpeg', 'webp', 'svg', 'pdf' handled by fig.write_image
