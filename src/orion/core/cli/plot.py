11058f0e1c1781ed2f60b5fb5f94abe2276fbf7d
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2021-04-07 12:12:38 -0400

    Rework plot cli to ease testing

--- src/orion/core/cli/plot.py
+++ src/orion/core/cli/plot.py
@@ -8,13 +8,23 @@ Exposes the interface for plotting for command-line usage.
 
 """
 import logging
+import os
+import sys
 
 import orion.core.io.experiment_builder as experiment_builder
+from orion.client.experiment import ExperimentClient
 from orion.core.cli import base as cli
 from orion.plotting.base import SINGLE_EXPERIMENT_PLOTS
+from orion.core.utils.exceptions import NoConfigurationError
 
 log = logging.getLogger(__name__)
 DESCRIPTION = "Produce plots for Oríon experiments"
+
+
+IMAGE_TYPES = ["png", "jpg", "jpeg", "webp", "svg", "pdf"]
+HTML_TYPES = ["html"]
+JSON_TYPES = ["json"]
+VALID_TYPES = IMAGE_TYPES + HTML_TYPES + JSON_TYPES
 
 
 def add_subparser(parser):
@@ -35,7 +45,7 @@ def add_subparser(parser):
         "--type",
         type=str,
         default="png",
-        choices=["png", "jpg", "jpeg", "webp", "svg", "pdf", "html", "json"],
+        choices=VALID_TYPES,
         help="type of plot to return. (default: png)",
     )
 
@@ -46,13 +56,13 @@ def add_subparser(parser):
         default="",
         help="path where plot is saved. "
         " Will override the `type` argument."
-        " (default is {exp.name}_{kind}.{type})",
+        " (default is {exp.name}-v{exp.version}_{kind}.{type})",
     )
 
     plot_parser.add_argument(
         "--scale",
         type=float,
-        default=None,
+        default=1,
         help="more pixels, but same proportions of the plot. "
         " Scale acts as multiplier on height and width of resulting image."
         " Overrides value of 'scale' in plotly.io.write_image.",
@@ -63,44 +73,48 @@ def add_subparser(parser):
     return plot_parser
 
 
+def infer_type(output, out_type):
+    if output:
+        ext = output.split(".")[-1]
+        if ext and ext not in VALID_TYPES:
+            log.warning(
+                "Overriding the `type` field with something from `output`, "
+                f"but we got the invalid value : {out_type}. Will revert to png."
+                f"Should be one of {VALID_TYPES}"
+            )
+        else:
+            out_type = ext
+
+    return out_type
+
+
+def get_output(experiment, output, kind, out_type):
+
+    if not output:
+        return f"{experiment.name}-v{experiment.version}_{kind}.{out_type}"
+    elif not output.endswith(f".{out_type}"):
+        return f"{output}.{out_type}"
+
+    return output
+
+
 def main(args):
     """Starts an application that will generate a plot."""
 
     # Note : If you specify no argument at all (except 'kind'),
     #        the default behavior is to plot "{experiment.name}_{kind}.png".
 
-    experiment = experiment_builder.build_from_args(args)
+    experiment = ExperimentClient(experiment_builder.get_from_args(args, mode="r"), None)
     output_plot = experiment.plot(kind=args["kind"])
 
-    valid_types = ["png", "jpg", "jpeg", "webp", "svg", "pdf", "html", "json"]
-    # 'png', 'jpg', 'jpeg', 'webp', 'svg', 'pdf' handled by fig.write_image
-    # 'html' handled by fig.write_html
-    # 'json' handled by just writing out the file
+    args["type"] = infer_type(args["output"], args["type"])
+    args["output"] = get_output(experiment, args["output"], args["kind"], args["type"])
 
-    if args["output"]:
-        # `output` was given, and we'll override `type`
-        args["type"] = args["output"].split(".")[-1]
-        assert args["type"] in valid_types, (
-            "Overriding the `type` field with something from `output`, "
-            f"but we got the invalid value : {args['type']} ."
-        )
-    else:
-        # `type` was given, and we'll pick a `output` based on the name
-
-        # Using `experiment.name` instead of args['name'] because it leaves
-        # orion the possibility of having inferred the name in other ways.
-        args["output"] = f"{experiment.name}_{args['kind']}.{args['type']}"
-
-    if args["type"] in ["png", "jpg", "jpeg", "webp", "svg", "pdf"]:
-        # a way to increase pixel counts without affecting the proportions
-        kwargs = {}
-        if args["scale"] is not None:
-            kwargs["scale"] = args["scale"]
-
-        output_plot.write_image(args["output"], **kwargs)
-    elif args["type"] in ["html"]:
+    if args["type"] in IMAGE_TYPES:
+        output_plot.write_image(args["output"], scale=args["scale"])
+    elif args["type"] in HTML_TYPES:
         output_plot.write_html(args["output"])
-    elif args["type"] in ["json"]:
+    elif args["type"] in JSON_TYPES:
         with open(args["output"], "w") as f_out:
             # Note that this is the content of the "body" in the WebApi.
             f_out.write(output_plot.to_json())
