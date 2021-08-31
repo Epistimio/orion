1c3c924b4882c2e2c760a40d40661f2624bad621
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-08-31 17:12:15 -0400

    Update profet_benchmark.py a bit
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- examples/benchmark/profet_benchmark.py
+++ examples/benchmark/profet_benchmark.py
@@ -8,16 +8,11 @@ import numpy as np
 import pandas as pd
 import plotly
 from orion.benchmark.assessment import AverageRank, AverageResult
-from orion.benchmark.assessment.warm_start_task_correlation import (
-    warm_start_task_correlation_figure,
-)
 from orion.benchmark.benchmark_client import get_or_create_benchmark
 from orion.benchmark.task import BaseTask, CarromTable, EggHolder, RosenBrock
 from orion.benchmark.task.profet import FcNetTask, ForresterTask, SvmTask, XgBoostTask
 from orion.benchmark.task.profet.profet_task import MetaModelTrainingConfig, ProfetTask
 from orion.benchmark.task.quadratics import QuadraticsTask
-from orion.benchmark.task.task import Task
-from orion.benchmark.warm_start_benchmark import WarmStartTaskCorrelationBenchmark
 from simple_parsing.helpers import choice, list_field
 
 logger = get_logger("orion.benchmark.task")
@@ -28,6 +23,7 @@ from orion.benchmark.task.profet import FcNetTask, ForresterTask, SvmTask, XgBoo
 # BUG: #629 (https://github.com/Epistimio/orion/issues/629)
 from orion.core.worker.primary_algo import PrimaryAlgo
 from orion.testing.space import build_space
+
 _ = PrimaryAlgo(space=build_space(), algorithm_config="random")
 
 
@@ -36,13 +32,8 @@ class ProfetExperimentConfig:
     """ Configuration option for the demo of the Profet tasks. """
 
     # The type of Profet task to create.
-    task_type: Type[Task] = choice(
-        {
-            "svm": SvmTask,
-            "fcnet": FcNetTask,
-            "xgboost": XgBoostTask,
-            "forrester": ForresterTask,
-        }
+    task_type: Type[BaseTask] = choice(
+        {"svm": SvmTask, "fcnet": FcNetTask, "xgboost": XgBoostTask, "forrester": ForresterTask,}
     )
 
     # Name of the experiment.
@@ -51,7 +42,7 @@ class ProfetExperimentConfig:
     profet_train_config: MetaModelTrainingConfig = MetaModelTrainingConfig()
 
     algorithms: List[str] = list_field("robo_gp", "robo_ablr", "robo_dngo")
-    
+
     # Number of repetitions for each experiment
     n_repetitions: int = 3
     # Optimization budget (max number of trials) for optimizing the task.
@@ -69,6 +60,9 @@ class ProfetExperimentConfig:
     # Path to the folder where figures are to be saved.
     figures_dir: Path = Path("figures")
 
+    input_dir: Path = Path("profet_data")
+    checkpoint_dir: Path = Path("profet_outputs")
+
     def __post_init__(self):
         simple_parsing_logger = get_logger("simple_parsing")
         if not self.debug:
@@ -78,10 +72,12 @@ class ProfetExperimentConfig:
 def main(config: ProfetExperimentConfig):
     if issubclass(config.task_type, ProfetTask):
         task = config.task_type(
-            task_id=0,
             max_trials=config.max_trials,
-            seed=config.seed,
+            task_id=0,
             train_config=config.profet_train_config,
+            input_dir=config.input_dir,
+            checkpoint_dir=config.checkpoint_dir,
+            seed=config.seed,
         )
     else:
         task = config.task_type(max_trials=config.max_trials)
@@ -89,14 +85,7 @@ def main(config: ProfetExperimentConfig):
     benchmark = get_or_create_benchmark(
         name=config.name,
         algorithms=config.algorithms,
-        targets=[
-            {
-                "assess": [AverageResult(config.n_repetitions)],
-                "task": [
-                    task,
-                ],
-            }
-        ],
+        targets=[{"assess": [AverageResult(config.n_repetitions)], "task": [task,],}],
         storage={
             "type": "legacy",
             "database": {"type": "pickleddb", "host": str(config.storage_pickle_path)},
@@ -111,37 +100,10 @@ def main(config: ProfetExperimentConfig):
     figures_dir = config.figures_dir / benchmark.name
     figures_dir.mkdir(exist_ok=True, parents=True)
 
-    save_path = "results_df.pkl"
-    # Use this to just re-create the plots.
-    if Path(save_path).exists() and False:  # FIXME: Debugging
-        print(f"Loading the existing results dataframe from path: {save_path}")
-        results_df: pd.DataFrame = pd.read_pickle(save_path)
-        benchmark.results_df = results_df
-        figures: List[plotly.graph_objects.Figure]
-        for algo_name in benchmark.algorithms:
-            figures = warm_start_task_correlation_figure(
-                df=results_df.loc[algo_name],
-                algorithm_name=algo_name,
-                # task_similarities=benchmark.task_similarities,
-                # target_task=target_task,
-                # source_tasks=benchmark.source_tasks,
-            )
-            for i, fig in enumerate(figures):
-                fig.show()
-                fig.write_image(str(figures_dir / f"{algo_name}_task_correlation.svg"))
+    benchmark.process(n_workers=1)
 
-    else:
-        benchmark.process(n_workers=1)
-
-        status = benchmark.status(False)
-        figures = benchmark.analysis()
-        # for figure in figures:
-        #     figure.show()
-
-        results_df = benchmark.results_df
-        print(f"Saving the results dataframe to path: {save_path}")
-        results_df.to_pickle(save_path)
-
+    status = benchmark.status(False)
+    print(status)
     # if all(isinstance(source_task, QuadraticsTask) for source_task in benchmark.source_tasks):
     figures = benchmark.analysis()
     # # # TODO: Instead of having like 30 figures, should try and create an interactive-ish
@@ -150,11 +112,9 @@ def main(config: ProfetExperimentConfig):
     # pio.renderers.default = "browser"
     for i, figure in enumerate(figures):
         figure.show()
-        figure.write_image(str(figures_dir / f"warm_start_fig_{i:02}.svg"))
-        figure.write_image(str(figures_dir / f"warm_start_fig_{i:02}.png"))
-        figure.write_html(
-            str(figures_dir / f"warm_start_fig_{i:02}.html"), include_plotlyjs="cdn"
-        )
+        figure.write_image(str(figures_dir / f"fig_{i:02}.svg"))
+        figure.write_image(str(figures_dir / f"fig_{i:02}.png"))
+        figure.write_html(str(figures_dir / f"fig_{i:02}.html"), include_plotlyjs="cdn")
 
 
 if __name__ == "__main__":
