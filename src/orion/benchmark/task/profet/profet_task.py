42b0857ba2066c6523fc9866f8b5fad668618c76
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-09-14 13:59:44 -0400

    Move compute_identity to orion.core.utils
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/benchmark/task/profet/profet_task.py
+++ src/orion/benchmark/task/profet/profet_task.py
@@ -45,6 +45,8 @@ from orion.core.io.space_builder import SpaceBuilder
 from orion.core.utils.format_trials import dict_to_trial, trial_to_tuple
 from orion.core.utils.points import flatten_dims
 from orion.core.worker.trial import Trial
+from orion.core.utils import compute_identity
+
 
 logger = get_logger(__name__)
 
@@ -506,30 +508,6 @@ def load_task_network(
 
     return network, h
 
-
-def compute_identity(size: int = 16, **sample) -> str:
-    """Compute a unique hash out of a dictionary
-
-    Parameters
-    ----------
-    size: int
-        size of the unique hash
-
-    **sample:
-        Dictionary to compute the hash from
-
-    """
-    sample_hash = hashlib.sha256()
-
-    for k, v in sorted(sample.items()):
-        sample_hash.update(k.encode("utf8"))
-
-        if isinstance(v, dict):
-            sample_hash.update(compute_identity(size, **v).encode("utf8"))
-        else:
-            sample_hash.update(str(v).encode("utf8"))
-
-    return sample_hash.hexdigest()[:size]
 
 
 def save_task_network(
--- src/orion/core/utils/__init__.py
+++ src/orion/core/utils/__init__.py
@@ -5,6 +5,7 @@ Package-wide useful routines
 
 """
 
+import hashlib
 import logging
 import os
 from abc import ABCMeta
@@ -149,3 +150,28 @@ class Factory(ABCMeta):
         error += "\nCurrently, there is an implementation for types:\n"
         error += str(sorted(cls.types.keys()))
         raise NotImplementedError(error)
+
+
+def compute_identity(size: int = 16, **sample) -> str:
+    """Compute a unique hash out of a dictionary
+
+    Parameters
+    ----------
+    size: int
+        size of the unique hash
+
+    **sample:
+        Dictionary to compute the hash from
+
+    """
+    sample_hash = hashlib.sha256()
+
+    for k, v in sorted(sample.items()):
+        sample_hash.update(k.encode("utf8"))
+
+        if isinstance(v, dict):
+            sample_hash.update(compute_identity(size, **v).encode("utf8"))
+        else:
+            sample_hash.update(str(v).encode("utf8"))
+
+    return sample_hash.hexdigest()[:size]
