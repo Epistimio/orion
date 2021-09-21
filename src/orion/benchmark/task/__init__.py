a5f88638aea868c79654c07f12ee44761ddfa8cf
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-09-21 13:02:31 -0400

    Remove comment from __init__.py
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/benchmark/task/__init__.py
+++ src/orion/benchmark/task/__init__.py
@@ -10,6 +10,3 @@ from .eggholder import EggHolder
 from .rosenbrock import RosenBrock
 
 __all__ = ["BaseTask", "RosenBrock", "Branin", "CarromTable", "EggHolder"]
-
-# IDEA: Add an external entry_point for Tasks, so that we can add the profet and OpenML
-# tasks as an optional extras_require.
