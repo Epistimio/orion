cb3567b6b299da0a32bd2b408c35368aaa6a626b
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-11-23 12:14:21 -0500

    -

--- src/orion/executor/joblib_backend.py
+++ src/orion/executor/joblib_backend.py
@@ -2,12 +2,12 @@ import warnings
 
 import joblib
 
-from orion.executor.multiprocess_backend import Multiprocess
+from orion.executor.multiprocess_backend import PoolExecutor
 
 
-class Joblib(Multiprocess):
+class Joblib(PoolExecutor):
     def __init__(self, n_workers=-1, backend="loky", **config):
         warnings.warn(
-            "Joblib backend is deprecated, use Multiprocess instead", DeprecationWarning
+            "Joblib backend is deprecated, use PoolExecutor instead", DeprecationWarning
         )
         super(Joblib, self).__init__(n_workers=n_workers)
