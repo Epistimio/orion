7ff0d88c3c45d576fed9772b5f0606d2b928d6a1
Bruno Carrez <bruno.carrez@mila.quebec>
2022-01-25 10:58:17 -0500

    fixed isort

--- tests/functional/commands/test_verbose_messages.py
+++ tests/functional/commands/test_verbose_messages.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Perform a functional test of the debug verbosity level."""
-import pytest
 import logging
 
+import pytest
+
 import orion.core.cli
+
 
 def test_version_print_debug_verbosity(caplog):
     """Tests that Orion version is printed in debug verbosity level"""
@@ -14,15 +16,14 @@ def test_version_print_debug_verbosity(caplog):
         orion.core.cli.main(["-vv"])
     assert "Orion version : " not in caplog.text
 
-    caplog.clear()    
+    caplog.clear()
     caplog.set_level(logging.DEBUG)
     with pytest.raises(SystemExit):
         orion.core.cli.main([""])
     assert "Orion version : " not in caplog.text
 
-    caplog.clear()    
+    caplog.clear()
     caplog.set_level(logging.DEBUG)
     with pytest.raises(SystemExit):
         orion.core.cli.main(["-vv"])
     assert "Orion version : " in caplog.text
-   
