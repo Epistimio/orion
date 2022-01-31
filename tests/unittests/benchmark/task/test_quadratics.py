d65d57958a4709e413b81912b0d4f3edd5d31571
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-01-31 13:16:27 -0500

    Update names of attributes in test as well
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- tests/unittests/benchmark/task/test_quadratics.py
+++ tests/unittests/benchmark/task/test_quadratics.py
@@ -12,11 +12,11 @@ class TestQuadraticsTask:
 
         task_a = self.task(max_trials=123, seed=seed)
         task_b = self.task(max_trials=123, seed=seed)
-        assert task_a.a2 == task_b.a2
-        assert task_a.a1 == task_b.a1
-        assert task_a.a0 == task_b.a0
+        assert task_a.a_2 == task_b.a_2
+        assert task_a.a_1 == task_b.a_1
+        assert task_a.a_0 == task_b.a_0
 
         task_c = self.task(max_trials=123, seed=seed + 1)
-        assert task_b.a2 != task_c.a2
-        assert task_b.a1 != task_c.a1
-        assert task_b.a0 != task_c.a0
+        assert task_b.a_2 != task_c.a_2
+        assert task_b.a_1 != task_c.a_1
+        assert task_b.a_0 != task_c.a_0
