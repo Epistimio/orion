1e1f8bb282ae694fab48bc1687ce8acdb84aeee2
Setepenre <pierre.delaunay@outlook.com>
2022-03-03 12:16:34 -0500

    -

--- src/orion/client/runner.py
+++ src/orion/client/runner.py
@@ -56,6 +56,7 @@ class Protected(object):
                 "Runner is executing inside a thread/subprocess, results could get lost "
                 "on interruptions"
             )
+            raise
 
         return self
 
