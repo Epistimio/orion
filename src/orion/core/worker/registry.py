ef16a91a4359634b2df1572c94d76ec0a8d59e3c
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-03-14 14:22:58 -0400

    Initial implementation of Registry classes
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/core/worker/registry.py
+++ src/orion/core/worker/registry.py
@@ -1,182 +1,108 @@
+""" Classes that serve as an in-memory storage of trials for the algorithms. """
 from __future__ import annotations
-from typing import *
+from collections import defaultdict
+from typing import Any, Iterator, Mapping
+from orion.core.worker.trial import Trial
 
 
+class Registry(Mapping[str, Trial]):
+    """In-memory container for the trials that the algorithm suggests/observes/etc."""
 
-# - Registry
-# - RegistryMapping
-# - Algo:
-#   - Refactor _trials_info -> Registry
-#   - state_dict/set_state
-# - AlgoWrapper
-#   - Init
-#   - suggest/observe
-#   - state_dict/set_state
-#   - is_done
-# - _instantiate_algo has to be changed to look like the `create_algo` from below
-# - Check-out Hyperband + PBT
-# 	- Keep get_id, gets ID of the transformed trial (without reversing the transform, original space)
-# - Commencer avec HyperBand ou PBT, vu que ce sont les plus chiants.
+    def __init__(self):
+        self._trials: dict[str, Trial] = {}
 
-
-
-class Registry:
-  	def __init__(self):
-      	self._trials_info: dict[str, Trial] = {}
-          
-    def __contains__(self, trial: str | Trial) -> bool:
-        trial_id = trial.id if isinstance(trial, Trial) else trial
-        return trial_id in self._trials_info
+    def __contains__(self, trial_or_id: str | Trial | Any) -> bool:
+        if isinstance(trial_or_id, Trial):
+            trial_id = trial_or_id.id
+        elif isinstance(trial_or_id, str):
+            trial_id = trial_or_id
+        else:
+            raise NotImplementedError(trial_or_id)
+        return trial_id in self._trials
 
     def __getitem__(self, item: str) -> Trial:
         if not isinstance(item, str):
             raise KeyError(item)
-        return self._trials_info[item]
+        return self._trials[item]
 
-    def state_dict(self):
-      return
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._trials)
 
-    def set_state(self, statedict):
-      pass
-        
-	def has_observed():
-    	pass
-      
-    def has_suggested(trial: Trial) -> bool:
-    	return trial in self
+    def __len__(self) -> int:
+        return len(self._trials)
 
-    def get_trial(self, id: str) -> Trial:
-    	return
-      
+    def state_dict(self) -> dict:
+        return self._trials
+
+    def set_state(self, statedict: dict) -> None:
+        self._trials = statedict["_trials"]
+
+    def has_suggested(self, trial: Trial) -> bool:
+        return trial.id in self
+
+    def has_observed(self, trial: Trial) -> bool:
+        return trial.id in self and self[trial.id].objective is not None
+
+    def get_trial(self, trial_id: str) -> Trial:
+        return self[trial_id]
+
     def register(self, trial: Trial) -> str:
-      	return id
+        """Register the given trial in the registry."""
+        if trial.id in self:
+            # TODO: Do we allow overwriting? Or maybe only if the status is updated?
+            # if self[trial.id].status != "completed"
+            raise RuntimeError(f"Trial {trial} is already registered.")
+        self._trials[trial.id] = trial
+        return trial.id
 
-      
-class RegistryMapping:
-	def __init__(self, orignal_registry, transformed_registry):
-		self.orignal_registry
-        self.original_registery = original_registery
+
+class RegistryMapping(Mapping[Trial, "list[Trial]"]):
+    """A map between the original and transformed registries.
+
+    This object is used in the `SpaceTransformAlgoWrapper` to check if a trial in the original space
+    has equivalent trials in the transformed space.
+
+    The goal is to make it so the algorithms don't have to care about the transforms/etc.
+    """
+
+    def __init__(self, original_registry: Registry, transformed_registry: Registry):
+        self.original_registry = original_registry
         self.transformed_registry = transformed_registry
         self._mapping: dict[str, set[str]] = defaultdict(set)
 
-    def state_dict(self):
-      return
-    
-    def set_state(self, statedict):
-      pass
-    
+    def state_dict(self) -> dict:
+        return {
+            "original_registry": self.original_registry.state_dict(),
+            "transformed_registry": self.transformed_registry.state_dict(),
+            "_mapping": self._mapping,
+        }
+
+    def set_state(self, statedict: dict):
+        self.original_registry.set_state(statedict["original_registry"])
+        self.transformed_registry.set_state(statedict["transformed_regsitry"])
+        self._mapping = statedict["_mapping"]
+
     def __contains__(self, trial: Trial):
         return trial in self.original_registry
-	
+
     def __getitem__(self, item: Trial) -> list[Trial]:
-      	if item.id not in self._mapping:
-          return []
-        return [self.transformed_registry.get_trial(transformed_id) for transformed_id in self._mapping[item.id]]
+        if item.id not in self._mapping:
+            raise KeyError(item)
+        transformed_trial_ids = self._mapping[item.id]
+        return [
+            self.transformed_registry[transformed_id]
+            for transformed_id in transformed_trial_ids
+        ]
 
     def get_trials(self, original_trial: Trial) -> list[Trial]:
-        return self[original_trial]
-        return list(self._mapping.get(original_trial, set()))
-      
+        """Return the registered transformed trials that map to the given trial in the original
+        space.
+        """
+        return self.get(original_trial, [])
+
     def register(self, original_trial: Trial, transformed_trial: Trial) -> str:
+        """Register an equivalence between the given original trial and the transformed trial."""
         # NOTE: Peut-être pas .id, faut voir comment on compute le ID.
-        self._mapping[original_trial.id].add(transformed_trial.id)
-      	return self.original_registry.register(original_trial)
-       
-        
-def create_algo(algo_constructor: type[Algorithm], space: Space, **algorithm_config) -> Algorithm:
-   requirements = backward.get_algo_requirements(algo_constructor) 
-   transformed_space = build_required_space(space, **requirements)
-   algorithm = algo_constructor(transformed_space, **algorithm_config)
-   return SpaceTransformAlgoWrapper(algorithm, space)
-
-
-class BaseAlgorithm(ABC):
-	def __init__(self, space: Space, **configuration):
-  		self._space = space
-      	self._registry = Registry()
-        self._kwargs = configuration
-Algorithm = BaseAlgorithm
-
-
-class SpaceTransformAlgoWrapper(Algorithm):
-  # ...
-  
-	def __init__(self, algorithm: Algorithm, space: Space):
-    	super().__init__(space=space)
-        self.algorithm = algorithm
-        self.registry_mapping = RegistryMapping(original_registry=self.registry, transformed_registry=self.algorithm.registry)
-  
-  	@property
-    def transformed_space(self) -> Space:
-        return self.algo.space
-  
-  	@property
-  	def original_space(self) -> Space:
-        return self.space
-  
-	def observe(self, trials: list[Trial]) -> None:
-        """Observe evaluated trials.
-
-        .. seealso:: `orion.algo.base.BaseAlgorithm.observe`
-        """
-        transformed_trials = []
-        for trial in trials:
-            self._verify_trial(trial, space=self.space)
-            # AVANT: transformed_trials.append(self.transformed_space.transform(trial))
-            transformed_trials.extend(self.registry_mapping.get_trials(trial))
-        self.algorithm.observe(transformed_trials)
-      
-	def suggest(self, num: int) -> list[Trial] | None:
-        """ Suggest some new trials if possible. """
-        transformed_trials = self.algorithm.suggest(num=num)
-        trials = []
-        for transformed_trial in transformed_trials:
-            original: Trial = self.transformed_space.reverse(transformed_trial)
-            if original not in self.registry:
-                trials.append(original)
-            self.registry_mapping.register(original, transformed_trial)
-            # self.registry_mapping[original].append(transformed_trial)
-		return trials
-      
-    @property
-    def is_done(self):
-        """Whether the algorithm is done and will not make further suggestions.
-
-        Return True, if an algorithm holds that there can be no further improvement.
-        By default, the cardinality of the specified search space will be used to check
-        if all possible sets of parameters has been tried.
-        """
-        if self.n_suggested >= self.original_space.cardinality:
-            return True
-
-        if self.n_observed >= getattr(self, "max_trials", float("inf")):
-            return True
-
-        return self.algorithm.is_done
-
-
-class MappingRegistry(Registry):
-	def __init__(self):
-    	super().__init__()
-        self._converted_trials = dict()
-        self._original_trials = dict()
-
-    def statedict(self):
-      return
-    
-    def setstate(self, statedict):
-      pass
-        
-	def has_observed(trial: Trial) -> bool:
-        transformed_trial = self.transformed_space.transform(trial)
-        return self.original_registry.has_observed(transformed_trial)
-    	pass
-      
-    def has_suggested():
-    	pass
-    
-    def get_trial():
-    	pass
-      
-    def register():
-      	return converted_trial
+        original_id = self.original_registry.register(original_trial)
+        self._mapping[original_id].add(transformed_trial.id)
+        return original_id
