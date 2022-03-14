998cd29afe34b575cf75b19cd9ad564facaa8741
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-03-14 15:19:01 -0400

    Adapt Random.suggest() for registry
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/algo/random.py
+++ src/orion/algo/random.py
@@ -66,8 +66,8 @@ class Random(BaseAlgorithm):
         """
         trials = []
         while len(trials) < num and not self.is_done:
-            seed = tuple(self.rng.randint(0, 1000000, size=3))
-            new_trial = self.format_trial(self.space.sample(1, seed=seed)[0])
+            # NOTE: space.sample() uses (and modifies) the random state here.
+            new_trial = self.space.sample(1, seed=self.rng)[0]
             if not self.has_suggested(new_trial):
                 self.register(new_trial)
                 trials.append(new_trial)
