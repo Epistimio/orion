031c98cf64ef5a479525e4df24f340ab38bb92c0
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-03-28 18:13:15 -0400

    Adding new tests in test_primary_algo.py
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- tests/unittests/core/test_primary_algo.py
+++ tests/unittests/core/test_primary_algo.py
@@ -3,13 +3,15 @@
 """Example usage and tests for :mod:`orion.core.worker.primary_algo`."""
 from __future__ import annotations
 
+import copy
 import typing
-from typing import Any, TypeVar
+from typing import Any, ClassVar, TypeVar
 
 import pytest
 
 from orion.algo.base import BaseAlgorithm, algo_factory
 from orion.algo.space import Space
+from orion.core.io.space_builder import SpaceBuilder
 from orion.core.utils import backward, format_trials
 from orion.core.worker.primary_algo import SpaceTransformAlgoWrapper, create_algo
 from orion.core.worker.transformer import build_required_space
@@ -24,7 +26,7 @@ def palgo(
     dumbalgo: type[DumbAlgo], space: Space, fixed_suggestion_value: Any
 ) -> SpaceTransformAlgoWrapper[DumbAlgo]:
     """Set up a SpaceTransformAlgoWrapper with dumb configuration."""
-    return create_algo(dumbalgo, space=space, value=fixed_suggestion_value)
+    return create_algo(algo_type=dumbalgo, space=space, value=fixed_suggestion_value)
 
 
 class TestSpaceTransformAlgoWrapperWraps:
@@ -49,7 +51,7 @@ class TestSpaceTransformAlgoWrapperWraps:
         )
         # transform point
         ttrial = tspace.transform(trial)
-
+        # TODO: https://github.com/Epistimio/orion/issues/804
         ttrial in tspace
 
         # Transformed point is not in original space
@@ -144,3 +146,159 @@ class TestSpaceTransformAlgoWrapperWraps:
         with pytest.raises(ValueError, match="not contained in space"):
             del fixed_suggestion._params[-1]
             palgo.judge(fixed_suggestion, 8)
+
+
+class StupidAlgo(BaseAlgorithm):
+    """A dumb algo that always returns the same trial."""
+
+    requires_type: ClassVar[str | None] = "real"
+    requires_shape: ClassVar[str | None] = "flattened"
+    requires_dist: ClassVar[str | None] = "linear"
+
+    def __init__(self, space: Space, fixed_suggestion: Trial):
+        super().__init__(space)
+        self.fixed_suggestion = fixed_suggestion
+        assert fixed_suggestion in space
+
+    def suggest(self, num):
+        self.register(self.fixed_suggestion)
+        return [self.fixed_suggestion]
+
+
+@pytest.fixture()
+def algo_wrapper():
+    """Fixture that creates the setup for the registration tests below."""
+    original_space = SpaceBuilder().build({"x": "loguniform(1, 100, discrete=True)"})
+    transformed_space = build_required_space(
+        original_space=original_space,
+        type_requirement=StupidAlgo.requires_type,
+        shape_requirement=StupidAlgo.requires_shape,
+        dist_requirement=StupidAlgo.requires_dist,
+    )
+
+    fixed_original = original_space.sample(1)[0]
+    fixed_transformed = transformed_space.transform(fixed_original)
+
+    algo = StupidAlgo(space=transformed_space, fixed_suggestion=fixed_transformed)
+
+    algo_wrapper = SpaceTransformAlgoWrapper(algo, space=original_space)
+    assert algo_wrapper.algorithm is algo
+    return algo_wrapper
+
+
+class TestRegistration:
+    """Tests for the new `registry` and `registry_mapping` of the transform algo wrapper."""
+
+    @pytest.mark.parametrize("original_status", ["completed", "broken", "pending"])
+    def test_suggest_equivalent_to_existing(
+        self, algo_wrapper: SpaceTransformAlgoWrapper[StupidAlgo], original_status: str
+    ):
+        """Test the case where the underlying algo suggests a transformed trial that is equivalent
+        to a previously-suggested trial in the original space.
+
+        In this case, the wrapped algo's `observe` method should be called inside the wrapper's
+        `suggest`, passing in the suggested transformed trial, but with the results and status
+        copied over from the existing `Trial`.
+        """
+
+        fixed_original = format_trials.dict_to_trial(
+            {"x": 10}, space=algo_wrapper.space
+        )
+        fixed_transformed = algo_wrapper.transformed_space.transform(fixed_original)
+        assert fixed_transformed.params == {"x": 2.302585092994046}
+        transformed_space = algo_wrapper.transformed_space
+
+        algo_wrapper.algorithm.fixed_suggestion = fixed_transformed
+
+        # Get a suggestion from the wrapper.
+        suggested_trials = algo_wrapper.suggest(1)
+        assert suggested_trials is not None
+        assert suggested_trials == [fixed_original]
+
+        assert fixed_original in algo_wrapper.registry
+        assert fixed_original in algo_wrapper.registry_mapping
+        assert algo_wrapper.registry_mapping[fixed_original] == [fixed_transformed]
+        assert algo_wrapper.has_suggested(fixed_original)
+        assert not algo_wrapper.has_observed(fixed_original)
+
+        assert algo_wrapper.algorithm.has_suggested(fixed_transformed)
+        assert not algo_wrapper.algorithm.has_observed(fixed_transformed)
+
+        # Now, add some results to the first suggestion, and have the wrapper observe it.
+        # NOTE: This might be the part that needs to be parametrized.
+        trial_with_results = copy.deepcopy(fixed_original)
+        trial_with_results._status = original_status
+        if original_status == "completed":
+            trial_with_results._results = [
+                Trial.Result(name="objective", type="objective", value=1)
+            ]
+
+        trial_with_results_transformed = transformed_space.transform(trial_with_results)
+        assert trial_with_results_transformed.results == trial_with_results.results
+
+        algo_wrapper.observe([trial_with_results])
+
+        if original_status in ["completed", "broken"]:
+            assert algo_wrapper.has_observed(trial_with_results)
+            assert algo_wrapper.algorithm.has_observed(trial_with_results_transformed)
+
+        # Pretty obvious, since this was true before observing. Checking again, just to be sure.
+        assert algo_wrapper.has_suggested(trial_with_results)
+        assert algo_wrapper.algorithm.has_suggested(trial_with_results_transformed)
+
+        # Create a Trial in the transformed space that will map to the same trial as the fixed
+        # suggestion in the original space.
+        equivalent_transformed = format_trials.dict_to_trial(
+            {"x": fixed_transformed.params["x"] + 1e-6},
+            space=transformed_space,
+        )
+        # Make sure that both are distinct, but map to the same trial in the original space.
+        assert equivalent_transformed != fixed_transformed
+        assert equivalent_transformed.params != fixed_transformed.params
+        equivalent_original = transformed_space.reverse(equivalent_transformed)
+        assert equivalent_original == fixed_original
+
+        # Update the dumb algo so that it now returns the equivalent transformed trial.
+        algo_wrapper.algorithm.fixed_suggestion = equivalent_transformed
+
+        # Get another suggestion from the wrapper. (the wrapped algo will return the equivalent)
+        new_suggested_trials = algo_wrapper.suggest(1)
+        assert new_suggested_trials is not None
+        # Here the algo wrapper should return an empty list, since the suggested trial is a
+        # duplicate of one that was already suggested.
+        assert new_suggested_trials == []
+
+        # The algo wrapper has suggested (and observed) a trial that is equivalent to this one.
+        if original_status in ["completed", "broken"]:
+            assert algo_wrapper.has_observed(equivalent_original)
+            assert algo_wrapper.algorithm.has_observed(equivalent_transformed)
+
+        assert algo_wrapper.has_suggested(equivalent_original)
+        assert algo_wrapper.algorithm.has_suggested(equivalent_transformed)
+
+    def test_observe_trial_not_suggested(
+        self, algo_wrapper: SpaceTransformAlgoWrapper[StupidAlgo]
+    ):
+        """Test the case where the wrapper observes a trial that hasn't been suggested by the
+        wrapped algorithm.
+        """
+        original_space = algo_wrapper.space
+        transformed_space = algo_wrapper.transformed_space
+        algo = algo_wrapper.algorithm
+
+        trial = original_space.sample()[0]
+        trial_with_results = copy.deepcopy(trial)
+        trial_with_results.status = "completed"
+        trial_with_results._results = [
+            Trial.Result(name="objective", type="objective", value=1)
+        ]
+        transformed_trial_with_results = transformed_space.transform(trial_with_results)
+
+        algo_wrapper.observe([trial_with_results])
+
+        assert algo_wrapper.has_observed(trial_with_results)
+        assert algo_wrapper.algorithm.has_observed(transformed_trial_with_results)
+
+        # TODO: Really unsure about these:
+        assert not algo_wrapper.has_suggested(trial_with_results)
+        assert not algo_wrapper.algorithm.has_suggested(transformed_trial_with_results)
