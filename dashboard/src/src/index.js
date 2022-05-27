e4a9567e414456475717fd5aebe7dbbf0afae1e7
notoraptor <stevenbocco@gmail.com>
2022-05-26 21:46:37 -0400

    Dashboard: - use axios instead of http to make API calls. react-scripts 5.0.1 dependencies makes `http` module unavailable, so, go back to axios. - remove useless packages enzyme-adapter-react-16, graphql and apollo - remove .env as it seems useless now

--- dashboard/src/src/index.js
+++ dashboard/src/src/index.js
@@ -1,9 +1,3 @@
-import 'core-js/modules/es7.array.includes';
-import 'core-js/modules/es6.array.fill';
-import 'core-js/modules/es6.string.includes';
-import 'core-js/modules/es6.string.trim';
-import 'core-js/modules/es7.object.values';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
@@ -11,22 +5,11 @@ import './style.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-  },
-});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
