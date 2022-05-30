9b85bb2a7be567211c8e2c236e9c3ca987330042
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2022-05-30 11:01:11 -0400

    Stop turning sphinx warnings into errors
    Why:
    
    With new sphinx version having non-escaped % raises warnings but it
    gives no indication to where these % are so that we can escape them.
    Stop turning sphinx warnings into errors until we have an automated way
    of fixing this...

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -85,7 +85,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
