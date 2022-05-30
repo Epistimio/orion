87041d62dd56364170c0a0834a1cefe60554ccff
Fabrice Normandin <normandf@mila.quebec>
2022-05-30 12:02:11 -0400

    Husk of unit tests for multi-task wrapper
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tests/unittests/core/worker/algo_wrappers/test_multi_task.py
+++ tests/unittests/core/worker/algo_wrappers/test_multi_task.py
@@ -1,3 +1,20 @@
+import pytest
+from orion.core.worker.knowledge_base import KnowledgeBase
 
 
+class DummyKnowledgeBase(KnowledgeBase):
+    ...
 
+
+@pytest.fixture()
+def knowledge_base():
+    pass
+
+
+class TestMultiTaskWrapper:
+    """Tests for the multi-task wrapper."""
+
+    def test_adds_task_id(self):
+        """Test that when an algo is wrapped with the multi-task wrapper, the trials it returns
+        with suggest() have an additional 'task-id' value.
+        """
