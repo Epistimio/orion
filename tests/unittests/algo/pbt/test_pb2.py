c63e3d3a3c8ec7a3ec4fb1ee609d54a17a7695f7
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2022-06-23 16:14:49 -0400

    isort

--- tests/unittests/algo/pbt/test_pb2.py
+++ tests/unittests/algo/pbt/test_pb2.py
@@ -9,7 +9,6 @@ import pytest
 from orion.algo.pbt.pb2_utils import HAS_PB2
 from orion.testing.algo import BaseAlgoTests, TestPhase
 
-
 if not HAS_PB2:
     pytest.skip("PB2 deps not installed", allow_module_level=True)
 
