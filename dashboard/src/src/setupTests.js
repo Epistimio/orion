d974ee32906c5f27839fb4b948df9b0cb5c90238
notoraptor <stevenbocco@gmail.com>
2022-07-05 14:20:20 -0400

    Re-increase timeouts - backend: 300s - axios: 400a - retry: 500s - unit test: 600s (unchanged)

--- dashboard/src/src/setupTests.js
+++ dashboard/src/src/setupTests.js
@@ -6,4 +6,4 @@ import '@testing-library/jest-dom';
 jest.setTimeout(600000);
 // Add necessary mock to test plotly (ref: https://github.com/plotly/react-plotly.js/issues/115#issuecomment-448687417)
 window.URL.createObjectURL = function() {};
-global.CONFIG_WAIT_FOR_LONG = { interval: 1000, timeout: 400000 };
+global.CONFIG_WAIT_FOR_LONG = { interval: 1000, timeout: 500000 };
