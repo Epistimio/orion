a5c65b2788f9bb6684f7c26e632ed9bcf4280c10
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-07-06 18:03:05 -0400

    Add a simple test for warm starting
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- tests/unittests/core/worker/warm_start/test_multi_task.py
+++ tests/unittests/core/worker/warm_start/test_multi_task.py
@@ -1,13 +1,17 @@
 """ Tests for the multi-task wrapper that is applied to regular algorithms when using a KB."""
 from __future__ import annotations
 
+import functools
+import inspect
 import random
-from typing import Callable
+from typing import Callable, Sequence
 
 import pytest
+from typing_extensions import ParamSpec
 
 from orion.algo.space import Space
-from orion.client import build_experiment
+from orion.client import build_experiment, workon
+from orion.client.experiment import ExperimentClient
 from orion.core.io.space_builder import SpaceBuilder
 from orion.core.worker.algo_wrappers.insist_suggest import InsistSuggest
 from orion.core.worker.algo_wrappers.space_transform import SpaceTransform
@@ -33,9 +37,10 @@ target_space = _space({"x": "uniform(0, 10)"})
 
 
 @pytest.fixture()
-def knowledge_base():
+def knowledge_base() -> KnowledgeBase:
+    """Dummy knowledge base fixture used for testing."""
+    # The KB gets populated with the previous experiments.
     previous_experiments = [
-        # build_experiment(name=f"foo-{i}", space=space_i, debug=True)
         ExperimentInfo.from_dict(
             build_experiment(name=f"foo-{i}", space=space_i, debug=True).configuration
         )
@@ -49,7 +54,12 @@ from orion.core.worker.warm_start import WarmStarteable
 
 
 class DummyWarmStarteableAlgo(Random, WarmStarteable):
-    def __init__(self, space, seed=None):
+    """A dummy warm-starteable algorithm.
+
+    Saves the warm start trials in an attribute for later inspection.
+    """
+
+    def __init__(self, space: Space, seed: int | Sequence[int] | None = None):
         super().__init__(space, seed)
         self._warm_start_trials = {}
 
@@ -61,6 +71,8 @@ class DummyWarmStarteableAlgo(Random, WarmStarteable):
 
 
 class TestCreateAlgo:
+    """Tests for the `create_algo` function. Checks that the right wrappers are applied."""
+
     def test_wrappers_are_applied(self, knowledge_base: KnowledgeBase):
         """Test that the MultiTaskWrapper is added whenever a knowledge base is passed and the
         algorithm is not warm-starteable.
@@ -131,3 +143,98 @@ class TestMultiTaskWrapper:
         unwrapped_algo = algo.unwrapped
         assert isinstance(unwrapped_algo, DummyWarmStarteableAlgo)
         assert unwrapped_algo._warm_start_trials
+
+
+def test_warm_starting_helps(knowledge_base: KnowledgeBase):
+    """Integration test. Shows that warm-starting helps in a simple task."""
+    target_space: Space = _space({"x": "uniform(0, 10)"})
+
+    # TODO
+    # experiment = build_experiment(
+    #     name="target",
+    #     space=target_space,
+    #     knowledge_base=knowledge_base,
+    #     algorithms=algo,
+    #     max_trials=100,
+    #     debug=True,
+    # )
+
+    from orion.algo.tpe import TPE
+
+    P = ParamSpec("P")
+
+    def with_results(objective_fn: Callable[P, float]) -> Callable[P, list[dict]]:
+        @functools.wraps(objective_fn)
+        def _with_results(*args: P.args, **kwargs: P.kwargs) -> list[dict]:
+            objective = objective_fn(*args, **kwargs)
+            return [dict(name="objective", type="objective", value=objective)]
+
+        return _with_results
+
+    def count_calls(objective_fn):
+        count = 0
+        signature = inspect.signature(objective_fn, follow_wrapped=True)
+
+        @functools.wraps(objective_fn)
+        def _count_calls(*args, **kwargs):
+            nonlocal count
+            count += 1
+            bound_args = signature.bind(*args, **kwargs)
+            bound_args.apply_defaults()
+            print(f"Starting Trial #{count} with {bound_args.arguments}")
+            results = objective_fn(*args, **kwargs)
+            print(f"Results: {results}")
+            return results
+
+        return _count_calls
+
+    def simple_quadratic(x: float) -> float:
+        return x**2 - 2 * x + 1
+
+    target_fn = count_calls(with_results(simple_quadratic))
+
+    cold_start = workon(
+        target_fn,
+        name="cold_start",
+        space=target_space,
+        max_trials=20,
+        algorithms=TPE,
+        knowledge_base=None,
+    )
+
+    def _get_objective(trial: Trial) -> float:
+        objective: Trial.Result | None = trial.objective
+        if objective is not None:
+            return objective.value
+        return float("inf")
+
+    def _get_best_trial(experiment: ExperimentClient) -> Trial:
+        trials: list[Trial] = experiment.fetch_trials_by_status(status="completed")
+        return min(trials, key=_get_objective)
+
+    cold_start_best_trial = _get_best_trial(cold_start)
+
+    # Now, add the first experiment to the knowledge base.
+    knowledge_base.add_experiment(cold_start)
+    hot_start = workon(
+        target_fn,
+        name="hot_start",
+        space=target_space,
+        # TODO: Should this max_trials include the number trials from warm starting?
+        # My take (@lebrice): Probably not. Doesn't make sense, since the KB is independent from
+        # the target experiment, and we shouldn't expect users to know exactly how many trials
+        # there are in the KB.
+        # IDEA: The MultiTaskWrapper could overwrite the `max_trials` property's setter, so that it
+        # remains unset, until we know how many trials we have from other experiments (call it N).
+        # Then, once that number is known, the max_trials on the wrapped algorithm could be set to
+        # `max_trials + N`.
+        # TODO: For now, I'm setting this to a larger number, just to make sure that we get some
+        # new trials from the target experiment.
+        max_trials=40,
+        algorithms=TPE,
+        knowledge_base=knowledge_base,
+    )
+    hot_start_best_trial = _get_best_trial(hot_start)
+    assert hot_start_best_trial.objective is not None
+    assert cold_start_best_trial.objective is not None
+    assert hot_start_best_trial.objective.value < cold_start_best_trial.objective.value
