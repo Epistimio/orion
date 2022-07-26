f04add147fd7c0e2805ab5ca632a9838f2b8ec41
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-07-26 14:04:52 -0400

    Add test for ExperimentConfig fields
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- tests/unittests/core/worker/warm_start/test_experiment_config.py
+++ tests/unittests/core/worker/warm_start/test_experiment_config.py
@@ -1,11 +1,19 @@
 """ Tests for the experiment config dataclasses. """
+
 from orion.client import build_experiment
 from orion.core.worker.experiment_config import ExperimentConfig
 
 
-def test_can_be_created_from_exp():
-    """An ExperimentConfig can be created from the `configuration` property of an Experiment."""
+def test_fields_match_exp_config_dict():
+    """Test that the annotations on `ExperimentConfig`'s match the Experiment config dict."""
     experiment = build_experiment(name="foo", space={"x": "uniform(0, 5)"}, debug=True)
     exp: ExperimentConfig = experiment.configuration
-    # TODO: Check that all fields match their type annotations.
     assert exp == experiment.configuration
+
+    actual_config_dict = experiment.configuration
+    # NOTE: We don't actually check the type of the value, because we would need to evaluate
+    # the forward references, and those might contain things like `int | None` that we can't
+    # evaluate.
+    assert set(actual_config_dict.keys()) == set(
+        ExperimentConfig.__annotations__.keys()
+    )
