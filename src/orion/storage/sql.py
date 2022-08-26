f820f2a21c7ce45cb8ce0255cf6029401c5589a9
Setepenre <pierre.delaunay@outlook.com>
2022-08-26 13:20:14 -0400

    Alchemy

---
+++ src/orion/storage/sql.py
@@ -0,0 +1,417 @@
+import contextlib
+import logging
+import datetime
+import pickle
+
+import sqlalchemy
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer, JSON
+from sqlalchemy import String, DateTime, select, delete, update
+from sqlalchemy.orm import declarative_base, Session
+
+import orion.core
+from orion.core.worker.trial import validate_status
+from orion.storage.base import BaseStorageProtocol, LockedAlgorithmState, get_trial_uid_and_exp, get_uid
+
+log = logging.getLogger(__name__)
+
+Base = declarative_base()
+
+# fmt: off
+class User(Base):
+    """Defines the User table"""
+    __tablename__ = "users"
+
+    uid         = Column(Integer, primary_key=True)
+    name        = Column(String(30))
+    token       = Column(String(30))
+    created_at  = Column(DateTime)
+    last_seen   = Column(DateTime)
+
+
+class Experiment(Base):
+    """Defines the Experiment table"""
+    __tablename__ = "experiments"
+
+    uid         = Column(Integer, primary_key=True)
+    name        = Column(String(30))
+    config      = Column(JSON)
+    version     = Column(Integer)
+    owner_id    = Column(Integer, ForeignKey("user.uid"), nullable=False)
+    datetime    = Column(DateTime)
+
+
+class Trial:
+    """Defines the Trial table"""
+    __tablename__ = "trial"
+
+    uid             = Column(Integer, primary_key=True)
+    experiment_id   = Column(Integer, ForeignKey("experiment.uid"), nullable=False)
+    owner_id        = Column(Integer, ForeignKey("user.uid"), nullable=False)
+    status          = Column(String(30))
+    results         = Column(JSON)
+    start_time      = Column(DateTime)
+    end_time        = Column(DateTime)
+    heartbeat       = Column(DateTime)
+
+
+class Algo:
+    """Defines the Algo table"""
+    __tablename__ = "algo"
+
+    uid             = Column(Integer, primary_key=True)
+    experiment_id   = Column(Integer, ForeignKey("experiment.uid"), nullable=False)
+    owner_id        = Column(Integer, ForeignKey("user.uid"), nullable=False)
+    configuration   = Column(JSON)
+    locked          = Column(Integer)
+    state           = Column(JSON)
+    heartbeat       = Column(DateTime)
+# fmt: on
+
+
+
+class SQLAlchemy(BaseStorageProtocol):  # noqa: F811
+    """Implement a generic protocol to allow Orion to communicate using
+    different storage backend
+
+    Parameters
+    ----------
+    uri: str
+        PostgreSQL backend to use for storage; the format is as follow
+         `protocol://[username:password@]host1[:port1][,...hostN[:portN]]][/[database][?options]]`
+
+    """
+
+    def __init__(self, uri):
+        self.engine = sqlalchemy.create_engine("", echo=True, future=True)
+
+        # Create the schema
+        Base.metadata.create_all(self.engine)
+
+        with Session(self.engine) as session:
+            stmt = select(User).where(User.token == self.token)
+            self.user = session.scalars(stmt).one()
+
+
+    # Experiment Operations
+    # =====================
+
+    def create_experiment(self, config):
+        """Insert a new experiment inside the database"""
+
+        with Session(self.engine) as session:
+            experiment = Experiment(
+                name=config['name'],
+                config=config,
+                onwer_id=self.user.uid,
+                version=0
+            )
+            session.add(experiment)
+            session.commit()
+
+    def delete_experiment(self, experiment, uid):
+        uid = get_uid(experiment, uid)
+
+        with Session(self.engine) as session:
+            stmt = delete(Experiment).where(Experiment.uid == uid)
+            session.execute(stmt)
+            session.commit()
+
+    def update_experiment(self, experiment=None, uid=None, where=None, **kwargs):
+        uid = get_uid(experiment, uid)
+
+        query = True
+        if uid is not None:
+            query = Experiment.uid == uid
+
+        query = query and self._to_query(Experiment, where)
+
+        with Session(self.engine) as session:
+            stmt = select(Experiment).where(query)
+            experiment = session.scalars(stmt).one()
+            experiment.config = kwargs
+            session.commit()
+
+    def fetch_experiments(self, query, selection=None):
+        query = self._to_query(query)
+
+        with Session(self.engine) as session:
+            stmt = select(Experiment).where(query)
+            experiments = session.scalars(stmt).all()
+
+        if selection is not None:
+            assert False, 'Not Implemented'
+
+        return experiments
+
+    # Benchmarks
+    # ==========
+
+
+    # Trials
+    # ======
+    def fetch_trials(self, experiment=None, uid=None, where=None):
+        uid = get_uid(experiment, uid)
+
+        query = True
+        if uid is not None:
+            query = Trial.experiment_id == uid
+
+        query = query and self._to_query(Trial, where)
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(query)
+            return session.scalars(stmt).all()
+
+    def register_trial(self, trial):
+        config = trial.to_dict()
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.uid == trial._id)
+            trial = session.scalars(stmt).one()
+            self._set_from_dict(trial, config)
+            session.commit()
+
+    def delete_trials(self, experiment=None, uid=None, where=None):
+        uid = get_uid(experiment, uid)
+
+        query = True
+        if uid is not None:
+            query = Trial.experiment_id == uid
+
+        query = query and self._to_query(Trial, where)
+
+        with Session(self.engine) as session:
+            stmt = delete(Trial).where(query)
+            session.execute(stmt)
+            session.commit()
+
+    def retrieve_result(self, trial, **kwargs):
+        return trial
+
+    def get_trial(self, trial=None, uid=None, experiment_uid=None):
+        trial_uid, experiment_uid = get_trial_uid_and_exp(trial, uid, experiment_uid)
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.experiment_id == experiment_uid and Trial.uid == trial_uid)
+            return session.scalars(stmt).one()
+
+    def update_trials(self, experiment=None, uid=None, where=None, **kwargs):
+        uid = get_uid(experiment, uid)
+        query = Trial.uid == trial._id and self._to_query(Trial, where)
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(query)
+            trials = session.scalars(stmt).all()
+            for trial in trials:
+                self._set_from_dict(trial, kwargs)
+            session.commit()
+
+        return trial
+
+    def update_trial(
+            self, trial=None, uid=None, experiment_uid=None, where=None, **kwargs
+        ):
+
+        trial_uid, experiment_uid = get_trial_uid_and_exp(trial, uid, experiment_uid)
+        query = Trial.uid == trial_uid and Trial.experiment_id == experiment_uid and self._to_query(where)
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(query)
+            trial = session.scalars(stmt).one()
+            self._set_from_dict(trial, kwargs)
+            session.commit()
+
+        return trial
+
+    def fetch_lost_trials(self, experiment):
+        heartbeat = orion.core.config.worker.heartbeat
+        threshold = datetime.datetime.utcnow() - datetime.timedelta(
+            seconds=heartbeat * 5
+        )
+
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.experiment_id == experiment._id and Trial.status == 'reserved' and Trial.heartbeat < threshold)
+            return session.scalars(stmt).all()
+
+    def push_trial_results(self, trial):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.experiment_id == trial.experiment and Trial.uid == trial.id and Trial.status == 'reserved')
+            trial = session.scalars(stmt).one()
+            self._set_from_dict(trial, trial.to_dict())
+            session.commit()
+
+        return trial
+
+    def set_trial_status(self, trial, status, heartbeat=None, was=None):
+        validate_status(status)
+        validate_status(was)
+
+        query = Trial.uid == trial.id # and Trial.experiment_id == trial.experiment
+        if was:
+            query = query and Trial.status == was
+
+        values = dict(status=status, experiment=trial.experiment)
+        if heartbeat:
+            values['heartbeat'] = heartbeat
+
+        with Session(self.engine) as session:
+            update(Trial).where(query).values(**values)
+            session.commit()
+
+    def fetch_pending_trials(self, experiment):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.status.in_("interrupted", "new", "suspended") and Trial.experiment_id == experiment._id)
+            return session.scalars(stmt).all()
+
+    def reserve_trial(self, experiment):
+        with Session(self.engine) as session:
+
+            with session.begin():
+                # not sure it prevents other worker from reserving the same trial
+                stmt = select(Trial).where(Trial.status.in_("interrupted", "new", "suspended") and Trial.experiment_id == experiment._id)
+                trial = session.scalars(stmt).one()
+
+                now = datetime.datetime.utcnow()
+                trial.status = 'reserved'
+                trial.start_time = now
+                trial.heartbeat= now
+
+        return trial
+
+    def fetch_trials_by_status(self, experiment, status):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.status == status and Trial.experiment_id == experiment._id)
+            return session.scalars(stmt).all()
+
+    def fetch_noncompleted_trials(self, experiment):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.status != 'completed' and Trial.experiment_id == experiment._id)
+            return session.scalars(stmt).all()
+
+    def count_completed(self, experiment):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.status == 'completed' and Trial.experiment_id == experiment._id)
+            return session.query(stmt).count()
+
+    def count_broken_trials(self, experiment):
+        with Session(self.engine) as session:
+            stmt = select(Trial).where(Trial.status == 'broken' and Trial.experiment_id == experiment._id)
+            return session.query(stmt).count()
+
+    def update_heartbeat(self, trial):
+        """Update trial's heartbeat"""
+
+        with Session(self.engine) as session:
+            update(Trial).where(Trial.uid == trial.id, Trial.status == 'reserved').values(heartbeat=datetime.datetime.utcnow())
+            session.commit()
+
+    # Algorithm
+    # =========
+    def initialize_algorithm_lock(self, experiment_id, algorithm_config):
+        with Session(self.engine) as session:
+            algo = Algo(
+                experiment_id=experiment_id,
+                onwer_id=self.user.uid,
+                configuration=algorithm_config,
+                locked=0,
+                heartbeat=datetime.datetime.utcnow()
+            )
+            session.add(algo)
+            session.commit()
+
+    def release_algorithm_lock(self, experiment=None, uid=None, new_state=None):
+        uid = get_uid(experiment, uid)
+
+        values = dict(
+            locked=0,
+            heartbeat=datetime.datetime.utcnow(),
+        )
+        if new_state is not None:
+            values["state"] = pickle.dumps(new_state)
+
+        with Session(self.engine) as session:
+            update(Algo).where(
+                Algo.experiment_id == uid and
+                Algo.locked == 1
+            ).values(**values)
+
+    def get_algorithm_lock_info(self, experiment=None, uid=None):
+        """See :func:`orion.storage.base.BaseStorageProtocol.get_algorithm_lock_info`"""
+        uid = get_uid(experiment, uid)
+
+        with Session(self.engine) as session:
+            stmt = select(Algo).where(Algo.experiment_id==uid)
+            algo = session.scalar(stmt).one()
+
+        return LockedAlgorithmState(
+            state=pickle.loads(algo.state) if algo.state is not None else None,
+            configuration=algo.configuration,
+            locked=algo.locked,
+        )
+
+    def delete_algorithm_lock(self, experiment=None, uid=None):
+        """See :func:`orion.storage.base.BaseStorageProtocol.delete_algorithm_lock`"""
+        uid = get_uid(experiment, uid)
+
+        with Session(self.engine) as session:
+            stmt = delete(Algo).where(Algo.experiment_id==uid)
+            session.execute(stmt)
+            session.commit()
+
+    @contextlib.contextmanager
+    def acquire_algorithm_lock(self, experiment=None, uid=None, timeout=60, retry_interval=1):
+        uid = get_uid(experiment, uid)
+
+        with Session(self.engine) as session:
+            stmt = update(Algo).where(Algo.experiment_id==uid, Algo.locked==0).values(
+                locked=1,
+                heartbeat=datetime.datetime.utcnow()
+            )
+            algo = session.scalar(stmt).one()
+            session.commit()
+
+        if algo is None:
+            return
+
+        algo_state = LockedAlgorithmState(
+            state=pickle.loads(algo.state) if algo.state is not None else None,
+            configuration=algo.configuration,
+            locked=True,
+        )
+
+        yield algo_state
+
+        self.release_algorithm_lock(uid, new_state=algo_state.state)
+
+
+    # Utilities
+    # =========
+    def _set_from_dict(self, obj, data, rest=None):
+        meta = dict()
+        while data:
+            k, v = data.popitem()
+
+            if hasattr(obj, k):
+                setattr(obj, k, v)
+            else:
+                meta[k] = v
+
+        if meta and rest:
+            setattr(obj, rest, meta)
+            return
+
+        if meta:
+            log.warning("Data was discarded %s", meta)
+
+    def _to_query(self, table, where):
+        query = True
+        for k, v in where.items():
+
+            if hash(table, k):
+                query = query and getattr(k) == v
+            else:
+                log.warning("constrained ignored %s = %s", k, v)
+
+        return query
