85bc9a8d92ca61b8262c242923eac74fe614ccd5
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-08-29 12:01:15 -0400

    Remove Registry from InsistSuggestWrapper
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/core/worker/algo_wrappers/insist_suggest.py
+++ src/orion/core/worker/algo_wrappers/insist_suggest.py
@@ -2,6 +2,7 @@
 """
 from __future__ import annotations
 
+import copy
 from logging import getLogger as get_logger
 
 from orion.algo.space import Space
@@ -17,6 +18,22 @@ class InsistSuggest(AlgoWrapper[AlgoT]):
     def __init__(self, space: Space, algorithm: AlgoT):
         super().__init__(space=space, algorithm=algorithm)
         self.max_suggest_attempts = 100
+
+    @property
+    def state_dict(self) -> dict:
+        # Gets the state of the wrapper + wrapped algorithm.
+        state_dict = super().state_dict
+        # Remove the registry from the state dict, since this wrapper doesn't do any
+        # transformation, and we'd like to avoid unnecessary IO.
+        state_dict.pop("registry")
+        return state_dict
+
+    def set_state(self, state_dict: dict) -> None:
+        # NOTE: We don't reset the state of the registry here.
+        # self.registry.set_state(state_dict["registry"])
+        self.algorithm.set_state(copy.deepcopy(state_dict["algorithm"]))
+        # Effectively copy the wrapped algorithm's registry into ours.
+        self.registry.set_state(self.algorithm.registry.state_dict)
 
     def suggest(self, num: int = 1) -> list[Trial]:
         trials: list[Trial] = []
