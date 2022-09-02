1e4a6c6c2035be077fc0dc0d1cca3c42e0399701
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2022-09-02 15:53:53 -0400

    clean up sphinx conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -116,47 +116,35 @@ default_role = "autolink"
 # a list of builtin themes.
 #
 html_theme = "pydata_sphinx_theme"
-html_theme_options = {
-    "logo": {},  # TODO
-    "github_url": "https://github.com/Epistimio/orion",
-    "collapse_navigation": True,
-    "show_nav_level": 2,
-    "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links.html"],
-    "switcher": {  # TODO in another PR. We need the version json file
-        "version_match": "v0.2.4",
-        "json_url": "https://numpy.org/doc/_static/versions.json",  # TODO
-    },
-    "left_sidebar_end": ["sidebar-ethical-ads"],
-}
-
-html_favicon = "_static/logo/orion_logo_bow_only_no_string.png"
-html_logo = "_static/logo/orion_logo_nogrid_72ppi.png"
-
-#     "canonical_url": "",
-#     "analytics_id": "",
-#     "logo_only": False,
-#     "display_version": True,
-#     "prev_next_buttons_location": "both",
-#     #  'style_external_links': False,
-#     #  'vcs_pageview_mode': '',
-#     # Toc options
-#     "collapse_navigation": False,
-#     "sticky_navigation": True,
-#     "navigation_depth": 4,
-#     #  'includehidden': False,
-#     #  'titles_only': False
-# }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_theme_options = {
+    # TODO: Add version for dark theme
+    # "logo": {
+    #     "image_light": "logo-light.png",
+    #     "image_dark": "logo-dark.png",
+    # },
+    "github_url": "https://github.com/Epistimio/orion",
+    "collapse_navigation": True,
+    "show_nav_level": 2,
+    "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links.html"],
+    # "switcher": {  # TODO in another PR. We need the version json file
+    #     "version_match": "v0.2.4",
+    #     "json_url": "https://numpy.org/doc/_static/versions.json",  # TODO
+    # },
+    "left_sidebar_end": ["sidebar-ethical-ads"],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_favicon = "_static/logo/orion_logo_bow_only_no_string.png"
+html_logo = "_static/logo/orion_logo_nogrid_72ppi.png"
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
