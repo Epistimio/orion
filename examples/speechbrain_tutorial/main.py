beaa3612cbe8e6aab54522bbbba14a22224e6930
Simnol22 <simonkirbi@hotmail.com>
2022-12-16 16:07:00 -0500

    all but report_objective

--- examples/speechbrain_tutorial/main.py
+++ examples/speechbrain_tutorial/main.py
@@ -7,6 +7,8 @@ from download_data import download
 from speechbrain.utils.distributed import run_on_main
 from train import ASR, dataio_prepare
 
+from orion.client import report_objective
+
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
@@ -33,25 +35,25 @@ if __name__ == "__main__":
         run_opts=run_opts,
         checkpointer=hparams["checkpointer"],
     )
-#
-#    # The `fit()` method iterates the training loop, calling the methods
-#    # necessary to update the parameters of the model. Since all objects
-#    # with changing state are managed by the Checkpointer, training can be
-#    # stopped at any point, and will be resumed on next call.
-#    print("Starting fit")
-#    asr_brain.fit(
-#        asr_brain.hparams.epoch_counter,
-#        datasets["train"],
-#        datasets["valid"],
-#        train_loader_kwargs=hparams["train_dataloader_opts"],
-#        valid_loader_kwargs=hparams["valid_dataloader_opts"],
-#    )
-#    print("Starting evaluate")
-#    # Load best checkpoint for evaluation
-#    valid_stats = asr_brain.evaluate(
-#        test_set=datasets["valid"],
-#        min_key="WER",
-#        test_loader_kwargs=hparams["valid_dataloader_opts"],
-#    )
+
+    # The `fit()` method iterates the training loop, calling the methods
+    # necessary to update the parameters of the model. Since all objects
+    # with changing state are managed by the Checkpointer, training can be
+    # stopped at any point, and will be resumed on next call.
+    print("Starting fit")
+    asr_brain.fit(
+        asr_brain.hparams.epoch_counter,
+        datasets["train"],
+        datasets["valid"],
+        train_loader_kwargs=hparams["train_dataloader_opts"],
+        valid_loader_kwargs=hparams["valid_dataloader_opts"],
+    )
+    print("Starting evaluate")
+    # Load best checkpoint for evaluation
+    valid_stats = asr_brain.evaluate(
+        test_set=datasets["valid"],
+        min_key="WER",
+        test_loader_kwargs=hparams["valid_dataloader_opts"],
+    )
 #
 #    report_objective(valid_stats)
