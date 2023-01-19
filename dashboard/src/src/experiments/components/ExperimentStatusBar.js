3413ad297e63fef8dcecc05ab8426e780854fc40
notoraptor <stevenbocco@gmail.com>
2023-01-19 12:47:58 -0500

    Implement lazy loading for nav bar.

--- dashboard/src/src/experiments/components/ExperimentStatusBar.js
+++ dashboard/src/src/experiments/components/ExperimentStatusBar.js
@@ -17,6 +17,55 @@ const StatusToProgress = {
   broken: 'danger',
 };
 
+/**
+ * Helper class to provide and cache experiment status.
+ */
+class ExperimentStatusProvider {
+  constructor() {
+    this.backend = new Backend(DEFAULT_BACKEND);
+    this.resolving = new Map();
+    this.resolved = new Map();
+  }
+  get(experiment) {
+    if (this.resolved.has(experiment)) {
+      console.log(`Already resolved: ${experiment}`);
+      return new Promise((resolve, reject) => {
+        const resolution = this.resolved.get(experiment);
+        if (resolution.error) {
+          reject(resolution.error);
+        } else {
+          resolve(resolution.data);
+        }
+      });
+    } else if (this.resolving.has(experiment)) {
+      console.log(`Still resolving: ${experiment}`);
+      return this.resolving.get(experiment);
+    } else {
+      console.log(`To resolve: ${experiment}`);
+      this.resolving.set(
+        experiment,
+        new Promise((resolve, reject) => {
+          this.backend
+            .query(`experiments/status/${experiment}`)
+            .then(status => {
+              this.resolving.delete(experiment);
+              this.resolved.set(experiment, { error: null, data: status });
+              resolve(status);
+            })
+            .catch(error => {
+              this.resolving.delete(experiment);
+              this.resolved.set(experiment, { error, data: null });
+              reject(error);
+            });
+        })
+      );
+      return this.resolving.get(experiment);
+    }
+  }
+}
+
+const EXPERIMENT_STATUS_PROVIDER = new ExperimentStatusProvider();
+
 export class ExperimentStatusBar extends React.Component {
   _isMounted = false;
   constructor(props) {
@@ -215,9 +264,7 @@ export class ExperimentStatusBar extends React.Component {
   }
   componentDidMount() {
     this._isMounted = true;
-    const backend = new Backend(DEFAULT_BACKEND);
-    backend
-      .query(`experiments/status/${this.props.name}`)
+    EXPERIMENT_STATUS_PROVIDER.get(this.props.name)
       .then(status => {
         if (this._isMounted) {
           this.setState({ status });
