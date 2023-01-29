d53c6e759efb0218219c47ac0ba37faffb5a0e2e
notoraptor <stevenbocco@gmail.com>
2023-01-29 17:46:21 -0500

    Prepare CI. Add a test for nav bar scrolling. Deactivate other dashboard SRC tests, as they won't currently pass.

--- dashboard/src/playwright.config.js
+++ dashboard/src/playwright.config.js
@@ -7,7 +7,6 @@ const { devices } = require('@playwright/test');
  */
 // require('dotenv').config();
 
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  * @type {import('@playwright/test').PlaywrightTestConfig}
@@ -21,7 +20,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
