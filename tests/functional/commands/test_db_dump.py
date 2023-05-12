e6b23fc06a28303219ba887bd9b5c83abecd5541
notoraptor <stevenbocco@gmail.com>
2023-05-12 09:45:38 -0400

    Test dump using a temporary output path for most unit tests except `test_dump_default`

--- tests/functional/commands/test_db_dump.py
+++ tests/functional/commands/test_db_dump.py
@@ -2,6 +2,7 @@
 """Perform functional tests for db dump."""
 
 import os
+import tempfile
 
 import pytest
 
@@ -50,41 +51,43 @@ def test_dump_overwrite(
     three_experiments_branch_same_name_trials_benchmarks, capsys, testing_helpers
 ):
     """Test dump with overwrite argument"""
-    assert not os.path.exists("dump.pkl")
-    try:
-        execute("db dump")
-        assert os.path.isfile("dump.pkl")
-        dumped_db = PickledDB("dump.pkl")
-        testing_helpers.assert_tested_db_structure(dumped_db)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dump_path = f"{tmp_dir}/dump.pkl"
+        try:
+            execute(f"db dump -o {dump_path}")
+            assert os.path.isfile(dump_path)
+            dumped_db = PickledDB(dump_path)
+            testing_helpers.assert_tested_db_structure(dumped_db)
 
-        # No overwrite by default. Should fail.
-        execute("db dump", assert_code=1)
-        captured = capsys.readouterr()
-        assert captured.err.strip().startswith(
-            "Error: Export output already exists (specify `--force` to overwrite) at"
-        )
+            # No overwrite by default. Should fail.
+            execute(f"db dump -o {dump_path}", assert_code=1)
+            captured = capsys.readouterr()
+            assert captured.err.strip().startswith(
+                "Error: Export output already exists (specify `--force` to overwrite) at"
+            )
 
-        # Overwrite. Should pass.
-        execute("db dump --force")
-        assert os.path.isfile("dump.pkl")
-        testing_helpers.assert_tested_db_structure(dumped_db)
-    finally:
-        clean_dump("dump.pkl")
+            # Overwrite. Should pass.
+            execute(f"db dump --force -o {dump_path}")
+            assert os.path.isfile(dump_path)
+            testing_helpers.assert_tested_db_structure(dumped_db)
+        finally:
+            clean_dump(dump_path)
 
 
 def test_dump_to_specified_output(
     three_experiments_branch_same_name_trials_benchmarks, capsys, testing_helpers
 ):
     """Test dump to a specified output file"""
-    dump_path = "test.pkl"
-    assert not os.path.exists(dump_path)
-    try:
-        execute(f"db dump -o {dump_path}")
-        assert os.path.isfile(dump_path)
-        dumped_db = PickledDB(dump_path)
-        testing_helpers.assert_tested_db_structure(dumped_db)
-    finally:
-        clean_dump(dump_path)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dump_path = f"{tmp_dir}/test.pkl"
+        assert not os.path.exists(dump_path)
+        try:
+            execute(f"db dump -o {dump_path}")
+            assert os.path.isfile(dump_path)
+            dumped_db = PickledDB(dump_path)
+            testing_helpers.assert_tested_db_structure(dumped_db)
+        finally:
+            clean_dump(dump_path)
 
 
 @pytest.mark.parametrize(
@@ -107,14 +110,14 @@ def test_dump_to_specified_output(
         ),
         (
             True,
-            "test.pkl",
+            True,
             None,
             True,
             "Error: Export output already exists (specify `--force` to overwrite)",
         ),
         (
             True,
-            "test.pkl",
+            True,
             True,
             False,
             "Error: No experiment found with query {'name': 'unknown-experiment'}. "
@@ -138,7 +141,7 @@ def test_dump_to_specified_output(
         ),
         (
             False,
-            "test.pkl",
+            True,
             None,
             False,
             "Error: No experiment found with query {'name': 'unknown-experiment'}. "
@@ -146,7 +149,7 @@ def test_dump_to_specified_output(
         ),
         (
             False,
-            "test.pkl",
+            True,
             None,
             False,
             "Error: No experiment found with query {'name': 'unknown-experiment'}. "
@@ -160,55 +163,58 @@ def test_dump_post_clean_on_error(
     """Test how dumped file is cleaned if dump fails."""
 
     # Prepare a command that will fail (by looking for unknown experiment)
-    command = ["db", "dump", "-n", "unknown-experiment"]
-    if output:
-        command += ["--output", output]
-    if overwrite:
-        command += ["--force"]
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        command = ["db", "dump", "-n", "unknown-experiment"]
+        if output:
+            output = f"{tmp_dir}/test.pkl"
+            command += ["--output", output]
+        if overwrite:
+            command += ["--force"]
 
-    expected_output = output or "dump.pkl"
+        expected_output = output or "dump.pkl"
 
-    # Create expected file if necessary
-    if already_exists:
-        assert not os.path.exists(expected_output), expected_output
-        with open(expected_output, "w"):
-            pass
-        assert os.path.isfile(expected_output)
+        # Create expected file if necessary
+        if already_exists:
+            assert not os.path.exists(expected_output), expected_output
+            with open(expected_output, "w"):
+                pass
+            assert os.path.isfile(expected_output)
 
-    # Execute command and expect it to fail
-    execute(" ".join(command), assert_code=1)
-    err = capsys.readouterr().err
+        # Execute command and expect it to fail
+        execute(" ".join(command), assert_code=1)
+        err = capsys.readouterr().err
 
-    # Check output error
-    assert err.startswith(error_message)
+        # Check output error
+        assert err.startswith(error_message)
 
-    # Check dump post-clean
-    if should_exist_after:
-        assert os.path.isfile(expected_output)
-        # Clean files anyway
-        os.unlink(expected_output)
-        if os.path.isfile(f"{expected_output}.lock"):
-            os.unlink(f"{expected_output}.lock")
-    else:
-        assert not os.path.exists(expected_output)
-        assert not os.path.exists(f"{expected_output}.lock")
+        # Check dump post-clean
+        if should_exist_after:
+            assert os.path.isfile(expected_output)
+            # Clean files anyway
+            os.unlink(expected_output)
+            if os.path.isfile(f"{expected_output}.lock"):
+                os.unlink(f"{expected_output}.lock")
+        else:
+            assert not os.path.exists(expected_output)
+            assert not os.path.exists(f"{expected_output}.lock")
 
 
 def test_dump_unknown_experiment(
     three_experiments_branch_same_name_trials_benchmarks, capsys
 ):
     """Test dump unknown experiment"""
-    assert not os.path.exists("dump.pkl")
-    try:
-        execute("db dump -n i-dont-exist", assert_code=1)
-        captured = capsys.readouterr()
-        assert captured.err.startswith(
-            "Error: No experiment found with query {'name': 'i-dont-exist'}. Nothing to dump."
-        )
-    finally:
-        # Output file is created as soon as dst storage object is created in dump_database()
-        # So, we still need to delete it here
-        clean_dump("dump.pkl")
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dump_path = f"{tmp_dir}/dump.pkl"
+        try:
+            execute(f"db dump -n i-dont-exist -o {dump_path}", assert_code=1)
+            captured = capsys.readouterr()
+            assert captured.err.startswith(
+                "Error: No experiment found with query {'name': 'i-dont-exist'}. Nothing to dump."
+            )
+        finally:
+            # Output file is created as soon as dst storage object is created in dump_database()
+            # So, we still need to delete it here
+            clean_dump(dump_path)
 
 
 @pytest.mark.parametrize(
@@ -237,21 +243,22 @@ def test_dump_experiment_test_single_exp(
     algo_state,
 ):
     """Test dump experiment test_single_exp"""
-    assert not os.path.exists("dump.pkl")
-    try:
-        command = "db dump -n test_single_exp"
-        if given_version is not None:
-            command += f" -v {given_version}"
-        execute(command)
-        assert os.path.isfile("dump.pkl")
-        dumped_db = PickledDB("dump.pkl")
-        testing_helpers.check_unique_import(
-            dumped_db,
-            "test_single_exp",
-            expected_version,
-            nb_trials=nb_trials,
-            nb_child_trials=nb_child_trials,
-            algo_state=algo_state,
-        )
-    finally:
-        clean_dump("dump.pkl")
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dump_path = f"{tmp_dir}/dump.pkl"
+        try:
+            command = f"db dump -n test_single_exp -o {dump_path}"
+            if given_version is not None:
+                command += f" -v {given_version}"
+            execute(command)
+            assert os.path.isfile(dump_path)
+            dumped_db = PickledDB(dump_path)
+            testing_helpers.check_unique_import(
+                dumped_db,
+                "test_single_exp",
+                expected_version,
+                nb_trials=nb_trials,
+                nb_child_trials=nb_child_trials,
+                algo_state=algo_state,
+            )
+        finally:
+            clean_dump(dump_path)
