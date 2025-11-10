5c68d74c59926699df62312a2d3bb190b2104172
notoraptor <stevenbocco@gmail.com>
2025-11-10 18:27:37 -0500

    dashboard: update test

--- dashboard/src/src/__tests__/queryServer.test.js
+++ dashboard/src/src/__tests__/queryServer.test.js
@@ -33,6 +33,6 @@ test('test backend bad call', () => {
   // Create backend with unreachable address.
   const backend = new Backend('http://localhost:1');
   return backend.query('').catch(error => {
-    expect(error.message).toMatch('/^connect ECONNREFUSED .+1:1$/');
+    expect(error.message).toMatch(/^connect ECONNREFUSED .+1:1$/);
   });
 });
