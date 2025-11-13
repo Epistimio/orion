4e3135a5ff91e0f2d023aa4b6a41b225badf5021
notoraptor <stevenbocco@gmail.com>
2025-11-13 14:32:44 -0500

    Fix usage of numpy random in MOFA

--- src/orion/algo/mofa/mofa.py
+++ src/orion/algo/mofa/mofa.py
@@ -28,6 +28,9 @@ from orion.core.worker.trial import Trial
 logger = logging.getLogger(__name__)
 
 
+_NUMPY_IS_OLD = version.parse(np.__version__) < version.parse("1.17.0")
+
+
 class MOFA(BaseAlgorithm):
     """
     MOdular FActorial Design (MOFA).
@@ -147,13 +150,19 @@ class MOFA(BaseAlgorithm):
             Integer seed for the random number generator.
 
         """
-        self.rng = np.random.RandomState(seed)
+        if _NUMPY_IS_OLD:
+            self.rng = np.random.RandomState(seed)
+        else:
+            self.rng = np.random.default_rng(seed)
 
     @property
     def state_dict(self) -> dict:
         """Return a state dict that can be used to reset the state of the algorithm."""
         state_dict = super().state_dict
-        state_dict["rng_state"] = self.rng.get_state()
+        if _NUMPY_IS_OLD:
+            state_dict["rng_state"] = self.rng.get_state()
+        else:
+            state_dict["rng_state"] = self.rng.bit_generator.state
         state_dict["completed_trials"] = copy.deepcopy(self.completed_trials)
         state_dict["duplicates"] = copy.deepcopy(self.duplicates)
         state_dict["converged"] = self.converged
@@ -175,7 +184,10 @@ class MOFA(BaseAlgorithm):
         """
         super().set_state(state_dict)
         self.seed_rng(0)
-        self.rng.set_state(state_dict["rng_state"])
+        if _NUMPY_IS_OLD:
+            self.rng.set_state(state_dict["rng_state"])
+        else:
+            self.rng.bit_generator.state = state_dict["rng_state"]
         self.completed_trials = copy.deepcopy(state_dict["completed_trials"])
         self.duplicates = copy.deepcopy(state_dict["duplicates"])
         self.converged = state_dict["converged"]
